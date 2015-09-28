3425769ade999ffbf3e0b47e582c4f382e2f712f
Leon Jacobs <leonja511@gmail.com>
2015-09-28 08:23:29 +0200

    Add a User view composer

--- src/resources/views/includes/sidebar.blade.php
+++ src/resources/views/includes/sidebar.blade.php
@@ -9,7 +9,7 @@
         <img src="http://image.eveonline.com/Character/1_128.jpg" class="img-circle" alt="User Image">
       </div>
       <div class="pull-left info">
-        <p>Player Name</p>
+        <p>{{ $user->name }}</p>
         <!-- Status -->
         <a href="#"><i class="fa fa-circle text-success"></i> Online</a>
       </div>
