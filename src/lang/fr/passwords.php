7d9633bbd4fb3002dfc62df863a3d4d34c265a4d
elfaus <e.elfaus@gmail.com>
2015-09-30 14:28:01 +0200

    add few translation according to recent commits

--- src/lang/fr/passwords.php
+++ src/lang/fr/passwords.php
@@ -18,5 +18,9 @@ return [
     'sent' => "Nous vous avons envoyé par mail le lien de réinitialisation du mot de passe !",
     'token' => "Ce jeton de réinitialisation du mot de passe n'est pas valide.",
     'user' => "Aucun utilisateur n'a été trouvé avec cette adresse e-mail.",
+    'reset_topic' => "Réinitialisation du mot de passe",
+    'reset_welcome' => "Veuillez indiquer l'adresse mail liée au compte pour lequel le mot de passe doit être réinitialisé.",
+    'reset_token_welcome' => "Veuillez saisir l'adresse mail et un nouveau mot de passe.",
+    'remember' => "Je me souviens de mes identifiants"
 
 ];
---
+++ src/lang/fr/sidebar.php
@@ -0,0 +1,11 @@
+<?php
+
+return [
+
+    'home'          => 'Accueil',
+    'configuration' => 'Configuration',
+    'users'         => 'Utilisateurs',
+    'permissions'   => 'Autorisations',
+    'other'         => 'Autre'
+
+];
