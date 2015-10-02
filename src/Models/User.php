89ca4e12938feec09e40786f048dbe4a351f813f
Leon Jacobs <leonja511@gmail.com>
2015-10-02 09:04:06 +0200

    Allow for the account status to be filled

--- src/Models/User.php
+++ src/Models/User.php
@@ -52,7 +52,7 @@ class User extends Model implements AuthenticatableContract, CanResetPasswordCon
      *
      * @var array
      */
-    protected $fillable = ['name', 'email', 'password'];
+    protected $fillable = ['name', 'email', 'password', 'active'];
 
     /**
      * The attributes excluded from the model's JSON form.
