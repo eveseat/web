04ca0ee720242e2ee82849d11e6f99d5deaf658e
Leon Jacobs <leonja511@gmail.com>
2015-10-30 15:36:02 +0200

    Add filter rules.

--- src/WebServiceProvider.php
+++ src/WebServiceProvider.php
@@ -88,6 +88,8 @@ class WebServiceProvider extends ServiceProvider
         $this->mergeConfigFrom(
             __DIR__ . '/Config/web.config.php', 'web.config');
         $this->mergeConfigFrom(
+            __DIR__ . '/Config/web.filter.rules.php', 'web.filter.rules');
+        $this->mergeConfigFrom(
             __DIR__ . '/Config/web.permissions.php', 'web.permissions');
     }
 
