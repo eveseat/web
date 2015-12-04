bc8e6eb0d0f18fe76e3678615075711923499ed0
Leon Jacobs <leonja511@gmail.com>
2015-12-04 16:42:04 +0200

    Start something for a dashboard

--- src/resources/views/home.blade.php
+++ src/resources/views/home.blade.php
@@ -1,9 +1,41 @@
-@extends('web::layouts.app')
+@extends('web::layouts.grids.12')
 
 @section('title', 'Home')
 @section('page_header', 'Home')
 @section('page_description', 'The home page')
 
-@section('content')
-  Soon™
+@section('full')
+
+  <div class="row">
+    <div class="col-md-6 col-sm-6 col-xs-12">
+      <div class="info-box">
+        <span class="info-box-icon bg-aqua"><i class="fa fa-server"></i></span>
+        <div class="info-box-content">
+          <span class="info-box-text">Online Players</span>
+          <span class="info-box-number">
+            {{ $server_status['onlinePlayers'] or 'Unknown' }}
+          </span>
+          <span class="text-muted">
+            Last Update: {{ human_diff($server_status['created_at']) }}
+          </span>
+        </div><!-- /.info-box-content -->
+      </div><!-- /.info-box -->
+    </div><!-- /.col -->
+    <div class="col-md-6 col-sm-6 col-xs-12">
+      <div class="info-box">
+        <span class="info-box-icon bg-green"><i class="fa fa-key"></i></span>
+        <div class="info-box-content">
+          <span class="info-box-text">Owned API Keys</span>
+          <span class="info-box-number">
+            {{ count(auth()->user()->keys) }}
+          </span>
+        </div><!-- /.info-box-content -->
+      </div><!-- /.info-box -->
+    </div><!-- /.col -->
+
+  </div>
+
+
+
 @stop
+
