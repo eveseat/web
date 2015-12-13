df6ddae68b4f3af673e3dcb4fd54abaf4dad12b4
Leon Jacobs <leonja511@gmail.com>
2015-12-13 19:23:43 +0200

    Version Bump

--- src/Config/web.config.php
+++ src/Config/web.config.php
@@ -21,7 +21,7 @@ with this program; if not, write to the Free Software Foundation, Inc.,
 
 return [
 
-    'version'         => '1.0.4',
+    'version'         => '1.0.5',
     'max_access_mask' => 1073741823
 
 ];
