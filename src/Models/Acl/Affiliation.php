d5376f9c3a6f5e72b596be607a057fb7dd91768e
Leon Jacobs <leonja511@gmail.com>
2016-01-26 16:58:45 +0200

    Update Copyright

--- src/Models/Acl/Affiliation.php
+++ src/Models/Acl/Affiliation.php
@@ -2,7 +2,7 @@
 /*
 This file is part of SeAT
 
-Copyright (C) 2015  Leon Jacobs
+Copyright (C) 2015, 2016  Leon Jacobs
 
 This program is free software; you can redistribute it and/or modify
 it under the terms of the GNU General Public License as published by
