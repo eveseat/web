bc1f5ac3f224068a673d77a1788c6368bbd3bbd4
Leon Jacobs <leonja511@gmail.com>
2016-07-23 15:15:51 +0200

    Version Bump

--- src/Config/web.config.php
+++ src/Config/web.config.php
@@ -21,7 +21,7 @@ with this program; if not, write to the Free Software Foundation, Inc.,
 
 return [
 
-    'version'                  => '1.0.29',
+    'version'                  => '1.0.30',
     'max_access_mask'          => 4294967295,
     'queue_status_update_time' => 10 * 1000, // milliseconds = seconds x 1,000
 
