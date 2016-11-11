33218ba28a7d473dcc34554fe68c84d964a35214
elfaus <elfaus@hotmail.fr>
2016-11-11 21:54:13 +0100

    Add two low fuel warning on starbase list - 72 hours fuel remaining or less : show label in yellow - 24 hours fuel remaining or less : show label in red

--- src/resources/views/corporation/starbase/summary.blade.php
+++ src/resources/views/corporation/starbase/summary.blade.php
@@ -38,12 +38,25 @@
                 </span>
               </span>
           </td>
-          <td>
+          <td data-order="{{ $starbase->starbaseName }}">
               <span data-toggle="tooltip"
                     title="" data-original-title="{{ $starbase->starbaseTypeName }}">
                 {!! img('type', $starbase->starbaseTypeID, 64, ['class' => 'img-circle eve-icon small-icon']) !!}
                 {{ $starbase->starbaseName }}
               </span>
+              @if($starbase->inSovSystem)
+              @if(carbon('now')->diffInHours(carbon('now')->addHours($starbase->fuelBlocks/ ceil($starbase->baseFuelUsage * 0.75))) < 24)
+              <span class="text-red pull-right"><i>{{ trans('web::seat.low_fuel') }} !</i></span>
+              @elseif(carbon('now')->diffInHours(carbon('now')->addHours($starbase->fuelBlocks/ ceil($starbase->baseFuelUsage * 0.75))) < 72)
+              <span class="text-yellow pull-right"><i>{{ trans('web::seat.low_fuel') }}</i></span>
+              @endif
+              @else
+              @if(carbon('now')->diffInHours(carbon('now')->addHours($starbase->fuelBlocks/$starbase->baseFuelUsage)) < 24)
+              <span class="text-red pull-right"><i>{{ trans('web::seat.low_fuel') }} !</i></span>
+              @elseif(carbon('now')->diffInHours(carbon('now')->addHours($starbase->fuelBlocks/$starbase->baseFuelUsage)) < 72)
+              <span class="text-yellow pull-right"><i>{{ trans('web::seat.low_fuel') }}</i></span>
+              @endif
+              @endif
           </td>
           <td>
             <b>{{ $starbase->moonName }}</b>
