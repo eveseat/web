799a7b931281660acc2b0935dd143090574b0d48
Loïc LEUILLIOT <e.elfaus@gmail.com>
2016-12-14 19:39:16 +0100

    add the ability to search on keyID field from ApiKey (#52)
    add key filtering into the global search feature

---
+++ src/resources/views/search/components/apikey.blade.php
@@ -0,0 +1,60 @@
+<div class="panel panel-default">
+  <div class="panel-heading">
+    <h3 class="panel-title">Api Key Results</h3>
+  </div>
+  <div class="panel-body">
+
+    <table class="table compact table-condensed table-hover table-responsive"
+           id="api-keys">
+      <thead>
+      <tr>
+        <th>{{ trans_choice('web::seat.key_id', 1) }}</th>
+        <th>{{ trans('web::seat.enabled') }}</th>
+        <th>{{ trans_choice('web::seat.type', 1) }}</th>
+        <th>{{ trans('web::seat.expiry') }}</th>
+        <th>{{ trans_choice('web::seat.character', 1) }}</th>
+        <th></th>
+      </tr>
+      </thead>
+    </table>
+
+  </div>
+</div>
+
+@push('javascript')
+
+<script>
+
+  $(function () {
+    $('table#api-keys').DataTable({
+      processing      : true,
+      serverSide      : true,
+      ajax            : '{{ route('api.key.list.data') }}',
+      columns         : [
+        {data: 'key_id', name: 'key_id'},
+        {
+          data: 'enabled', name: 'enabled', render: function (data) {
+          if (data == 1) return 'Yes';
+          if (data == 0) return 'No';
+        }
+        },
+        {data: 'info.type', name: 'info.type'},
+        {data: 'info.expires', name: 'info.expires'},
+        {data: 'characters', name: 'characters', orderable: false},
+        {data: 'actions', name: 'actions', orderable: false},
+      ],
+      "fnDrawCallback": function () {
+        $(document).ready(function () {
+          $("img").unveil(100);
+        });
+      },
+      'search'        : {
+        'search': '{{ $query }}'
+      },
+      order           : [[0, "asc"]]
+    });
+  });
+
+</script>
+
+@endpush
