d08077fab03ab31f385970125832baf8512c056e
Leon Jacobs <leonja511@gmail.com>
2016-12-28 07:43:17 +0200

    Check that key info exists before attempting to set it

--- src/Http/Controllers/Api/KeyController.php
+++ src/Http/Controllers/Api/KeyController.php
@@ -326,8 +326,13 @@ class KeyController extends Controller
             ->take(50)
             ->get();
 
-        // Get worker information.
-        $key_type = $key->info->type == 'Corporation' ? 'corporation' : 'character';
+        // Get worker information. If we dont have key info yet,
+        // just default to a character. It will update later.
+        if ($key->info)
+            $key_type = $key->info->type == 'Corporation' ? 'corporation' : 'character';
+        else
+            $key_type = 'character';
+
         $available_workers = config('eveapi.worker_groups');
         $current_workers = $key->api_call_constraints;
 
