4f5af789b6e871dd78bda8e92d68a82409d3f4ac
BenHUET <benjamin.huet.1992@gmail.com>
2017-02-02 05:19:37 +0100

    Allow to add custom content from a child template to <head> (#64)
    * add stack css
    
    * added stack to login layout and changed named

--- src/resources/views/layouts/app.blade.php
+++ src/resources/views/layouts/app.blade.php
@@ -27,6 +27,8 @@
   <!-- SeAT CSS -->
   <link rel="stylesheet" href="{{ asset('web/css/seat.css') }}">
 
+  @stack('head')
+
   <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
   <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
   <!--[if lt IE 9]>
