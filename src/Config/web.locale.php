758cb4ce5f8f72f13f19841cd743fbf1a92af832
Viola <viola.r@linux.com>
2017-07-02 05:50:12 -0500

    add language Chinese (#79)
    * add language Chinese
    
    * correct some Chinese translation

--- src/Config/web.locale.php
+++ src/Config/web.locale.php
@@ -28,6 +28,10 @@ return [
             'full'  => 'Afrikaans',
         ],
         [
+            'short' => 'cn',
+            'full'  => 'Chinese',
+        ],
+        [
             'short' => 'en',
             'full'  => 'English',
         ],
