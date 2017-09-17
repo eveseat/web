036bce9674d166356818f1c3dd0a88367b3a45c4
Leon Jacobs <leonja511@gmail.com>
2017-09-17 14:20:59 +0200

    Version bump

--- src/Config/web.config.php
+++ src/Config/web.config.php
@@ -22,7 +22,7 @@
 
 return [
 
-    'version'                  => '2.0.17',
+    'version'                  => '2.0.18',
     'max_access_mask'          => 4294967295,
     'queue_status_update_time' => 10 * 1000, // milliseconds = seconds x 1,000
 
