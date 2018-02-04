5fb54d36fd23f9d96c31b9fff765c9aa4107e97c
Loïc LEUILLIOT <e.elfaus@gmail.com>
2018-02-04 22:19:02 +0100

    upgrade character standing view to ESI

--- src/resources/views/character/standings.blade.php
+++ src/resources/views/character/standings.blade.php
@@ -18,28 +18,36 @@
           <th>{{ trans('web::seat.standings') }}</th>
         </tr>
 
-        @foreach($standings->unique('type')->groupBy('type') as $type => $data)
+        @foreach($standings->unique('from_type')->groupBy('from_type') as $type => $data)
 
           <tr class="active">
             <td colspan="2">
+              @if($type == 'npc_corp')
+              <b>Corporation NPC</b>
+              @else
               <b>{{ ucfirst($type) }}</b>
+              @endif
             </td>
           </tr>
 
-          @foreach($standings->where('type', $data[0]->type) as $standing)
+          @foreach($standings->where('from_type', $data[0]->from_type) as $standing)
 
             <tr>
               <td>
-                {!! img('auto', $standing->fromID, 64, ['class' => 'img-circle eve-icon small-icon']) !!}
-                {{ $standing->fromName }}
+                {!! img('auto', $standing->from_id, 64, ['class' => 'img-circle eve-icon small-icon']) !!}
+                @if(is_null($standing->factionName))
+                <span rel="id-to-name">{{ $standing->from_id }}</span>
+                @else
+                {{ $standing->factionName }}
+                @endif
               </td>
               <td>
                 <span class="
                   @if($standing->standing > 0)
-                        text-success
-                      @else
-                        text-danger
-                      @endif">
+                  text-success
+                  @else
+                  text-danger
+                  @endif">
                   {{ $standing->standing }}
                 </span>
               </td>
