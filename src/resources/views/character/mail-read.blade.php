1c20a587d3deea6c3300bbb42772e98439e9f3fc
Loïc LEUILLIOT <e.elfaus@gmail.com>
2018-02-09 23:46:28 +0100

    upgrade character mails to ESI models

--- src/resources/views/character/mail-read.blade.php
+++ src/resources/views/character/mail-read.blade.php
@@ -11,50 +11,60 @@
     </div>
     <div class="panel-body">
 
-      <p>
       <h4>
         <i class="fa fa-envelope-o"></i>
-        {{ $message->title }}
+        {{ $message->subject }}
       </h4>
-      </p>
 
       <p>
-      <ul class="list-unstyled">
-        <li>
-          <b>Sent:</b>
-          {{ $message->sentDate }} ({{ human_diff($message->sentDate) }})
-        </li>
-        <li>
-          <b>From:</b>
-          <a href="{{ route('character.view.sheet', ['character_id' => $message->senderID]) }}">
-            {!! img('character', $message->senderID, 64, ['class' => 'img-circle eve-icon small-icon']) !!}
-            {{ $message->senderName }}
-          </a>
-        </li>
-
-        @if($message->toCorpOrAllianceID)
+        <ul class="list-unstyled">
           <li>
-            <b>To Corporation / Alliance:</b>
-            {!! img('auto', $message->toCorpOrAllianceID, 64, ['class' => 'img-circle eve-icon small-icon']) !!}
-            <span rel="id-to-name">{{ $message->toCorpOrAllianceID }}</span>
+            <b>Sent:</b>
+            {{ $message->timestamp }} ({{ human_diff($message->timestamp) }})
           </li>
-        @endif
+          <li>
+            <b>From:</b>
+            <a href="{{ route('character.view.sheet', ['character_id' => $message->from]) }}">
+              {!! img('character', $message->from, 64, ['class' => 'img-circle eve-icon small-icon']) !!}
+              <span rel="id-to-name">{{ $message->from }}</span>
+            </a>
+          </li>
 
-        @if($message->toCharacterIDs)
+          @if($message->recipients->where('recipient_type', 'alliance')->count() > 0)
+          <li>
+            <b>To Alliance:</b>
+            @foreach($message->recipients->where('recipient_type', 'alliance') as $alliance)
+            {!! img('alliance', $alliance->recipient_id, 64, ['class' => 'img-circle eve-icon small-icon']) !!}
+            <span rel="id-to-name">{{ $alliance->recipient_id }}</span>
+            @endforeach
+          </li>
+          @endif
+
+          @if($message->recipients->where('recipient_type', 'corporation')->count() > 0)
+          <li>
+            <b>To Corporation:</b>
+            @foreach($message->recipients->where('recipient_type', 'corporation') as $corporation)
+            {!! img('corporation', $corporation->recipient_id, 64, ['class' => 'img-circle eve-icon small-icon']) !!}
+            <span rel="id-to-name">{{ $corporation->recipient_id }}</span>
+            @endforeach
+          </li>
+          @endif
+
+          @if($message->recipients->where('recipient_type', 'character')->count() > 0)
           <li>
             <b>To Characters:</b>
 
-            @foreach(explode(',', $message->toCharacterIDs) as $char_id)
-              <a href="{{ route('character.view.sheet', ['character_id' => $char_id]) }}">
-                {!! img('character', $char_id, 64, ['class' => 'img-circle eve-icon small-icon']) !!}
-                <span rel="id-to-name">{{ $char_id }}</span>
-              </a>
+            @foreach($message->recipients->where('recipient_type', 'character') as $character)
+            <a href="{{ route('character.view.sheet', ['character_id' => $character->recipient_id]) }}">
+              {!! img('character', $character->recipient_id, 64, ['class' => 'img-circle eve-icon small-icon']) !!}
+              <span rel="id-to-name">{{ $character->recipient_id }}</span>
+            </a>
             @endforeach
 
           </li>
-        @endif
+          @endif
 
-      </ul>
+        </ul>
       </p>
 
       <p>
@@ -65,7 +75,7 @@
 
         @else
 
-          {!! clean_ccp_html($message->body) !!}
+          {!! clean_ccp_html($message->body->body) !!}
 
         @endif
 
--- src/resources/views/character/partials/mailread.blade.php
+++ src/resources/views/character/partials/mailread.blade.php
@@ -1,4 +1,4 @@
-<a href="{{ route('character.view.mail.read', ['characterID' => $row->characterID, 'message_id' => $row->messageID]) }}"
+<a href="{{ route('character.view.mail.read', ['characterID' => $row->character_id, 'message_id' => $row->mail_id]) }}"
    class="btn btn-xs btn-primary">
   <i class="fa fa-envelope"></i>
   Read
