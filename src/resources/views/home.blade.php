015fcc6bd33886d0f0259c773d87cb2145090ece
Leon Jacobs <leonja511@gmail.com>
2018-05-18 23:24:49 +0200

    Reshuffle the dashboard widgets.

--- src/resources/views/home.blade.php
+++ src/resources/views/home.blade.php
@@ -1,13 +1,13 @@
-@extends('web::layouts.grids.12')
+@extends('web::layouts.grids.6-6')
 
 @section('title', trans('web::seat.home'))
 @section('page_header', trans('web::seat.home'))
 @section('page_description', trans('web::seat.dashboard'))
 
-@section('full')
+@section('left')
 
   <div class="row">
-    <div class="col-md-6 col-sm-6 col-xs-12">
+    <div class="col-md-12 col-sm-6 col-xs-12">
 
       <div class="info-box">
         <span class="info-box-icon bg-aqua"><i class="fa fa-server"></i></span>
@@ -21,9 +21,6 @@
           </span>
         </div><!-- /.info-box-content -->
       </div><!-- /.info-box -->
-
-    </div><!-- /.col -->
-    <div class="col-md-6 col-sm-6 col-xs-12">
 
       <div class="info-box">
         <span class="info-box-icon bg-green"><i class="fa fa-key"></i></span>
@@ -39,24 +36,8 @@
   </div>
 
   <div class="row">
-    <div class="col-md-6 col-sm-6 col-xs-12">
 
-      <div class="box box-info">
-        <div class="box-header with-border">
-          <h3 class="box-title">Concurrent Player Count</h3>
-        </div>
-        <div class="box-body">
-          <div class="chart">
-
-            <canvas id="serverstatus" style="height: 249px; width: 555px;" height="265" width="1110"></canvas>
-
-          </div>
-        </div>
-        <!-- /.box-body -->
-      </div>
-
-    </div><!-- /.col -->
-    <div class="col-md-6 col-sm-6 col-xs-12">
+    <div class="col-md-12 col-sm-6 col-xs-12">
 
       <div class="info-box">
         <span class="info-box-icon bg-blue"><i class="fa fa-money"></i></span>
@@ -79,38 +60,8 @@
       </div><!-- /.info-box -->
 
     </div><!-- /.col -->
-  </div>
 
-  <div class="row">
-
-    <!-- skills graphs -->
-    @if(auth()->user()->name != 'admin')
-    <div class="col-md-6 col-sm-6 col-xs-12">
-
-      <div class="box">
-        <div class="box-header with-border">
-          <h3 class="box-title">
-            {!! img('character', auth()->user()->character_id, 64, ['class' => 'img-circle eve-icon small-icon']) !!}
-            {{ trans('web::seat.main_char_skills', ['character_name' => auth()->user()->name]) }}
-          </h3>
-        </div>
-        <div class="box-body">
-
-          <h4 class="box-title">{{ trans('web::seat.main_char_skills_per_level') }}</h4>
-          <div class="chart">
-            <canvas id="skills-level" height="230" width="1110"></canvas>
-          </div>
-
-          <h4 class="box-title">{{ trans('web::seat.main_char_skills_coverage') }}</h4>
-          <div class="chart">
-            <canvas id="skills-coverage" height="600" width="1110"></canvas>
-          </div>
-        </div>
-      </div>
-
-    </div><!-- /.col -->
-    @endif
-    <div class="col-md-6 col-sm-6 col-xs-12">
+    <div class="col-md-12 col-sm-6 col-xs-12">
 
       <div class="info-box">
         <span class="info-box-icon bg-red"><i class="fa fa-space-shuttle"></i></span>
@@ -121,7 +72,6 @@
           </span>
         </div><!-- /.info-box-content -->
       </div><!-- /.info-box -->
-
 
       @if($newest_mail->count() > 0)
 
@@ -176,51 +126,105 @@
 
 @stop
 
+@section('right')
+
+  <div class="row">
+
+    <div class="col-md-12 col-sm-6 col-xs-12">
+
+      <div class="box box-info">
+        <div class="box-header with-border">
+          <h3 class="box-title">Concurrent Player Count</h3>
+        </div>
+        <div class="box-body">
+          <div class="chart">
+
+            <canvas id="serverstatus" style="height: 249px; width: 555px;" height="265" width="1110"></canvas>
+
+          </div>
+        </div>
+        <!-- /.box-body -->
+      </div>
+    </div>
+
+    <!-- skills graphs -->
+    @if(auth()->user()->name != 'admin')
+      <div class="col-md-12 col-sm-6 col-xs-12">
+
+        <div class="box">
+          <div class="box-header with-border">
+            <h3 class="box-title">
+              {!! img('character', auth()->user()->character_id, 64, ['class' => 'img-circle eve-icon small-icon']) !!}
+              {{ trans('web::seat.main_char_skills', ['character_name' => auth()->user()->name]) }}
+            </h3>
+          </div>
+          <div class="box-body">
+
+            <h4 class="box-title">{{ trans('web::seat.main_char_skills_per_level') }}</h4>
+            <div class="chart">
+              <canvas id="skills-level" height="230" width="1110"></canvas>
+            </div>
+
+            <h4 class="box-title">{{ trans('web::seat.main_char_skills_coverage') }}</h4>
+            <div class="chart">
+              <canvas id="skills-coverage" height="600" width="1110"></canvas>
+            </div>
+          </div>
+        </div>
+
+      </div><!-- /.col -->
+    @endif
+
+  </div>
+
+@stop
+
 @push('javascript')
-<script type="text/javascript">
-  $.get("{{ route('home.chart.serverstatus') }}", function (data) {
+  <script type="text/javascript">
+    $.get("{{ route('home.chart.serverstatus') }}", function (data) {
 
-    new Chart($("canvas#serverstatus"), {
-      type   : 'line',
-      data   : data,
-      options: {
-        legend: {
-          display: false
-        },
-        scales: {
-          xAxes: [{
-            display: false
-          }]
-        }
-      }
-    });
-  });
-
-  if ($('canvas#skills-level').length)
-    $.get("{{ route('character.view.skills.graph.level', ['character_id' => auth()->user()->character_id]) }}", function (data) {
-      new Chart($("canvas#skills-level"), {
-        type: 'pie',
-        data: data
-      });
-    });
-
-  if ($('canvas#skills-coverage').length)
-    $.get("{{ route('character.view.skills.graph.coverage', ['character_id' => auth()->user()->character_id]) }}", function (data) {
-      new Chart($('canvas#skills-coverage'), {
-        type   : 'radar',
+      new Chart($("canvas#serverstatus"), {
+        type   : 'line',
         data   : data,
         options: {
-          scale : {
-            ticks: {
-              beginAtZero: true,
-              max        : 100
-            }
-          },
           legend: {
             display: false
+          },
+          scales: {
+            xAxes: [{
+              display: false
+            }]
           }
         }
       });
     });
-</script>
+
+    if ($('canvas#skills-level').length)
+      $.get("{{ route('character.view.skills.graph.level', ['character_id' => auth()->user()->character_id]) }}", function (data) {
+        new Chart($("canvas#skills-level"), {
+          type: 'pie',
+          data: data
+        });
+      });
+
+    if ($('canvas#skills-coverage').length)
+      $.get("{{ route('character.view.skills.graph.coverage', ['character_id' => auth()->user()->character_id]) }}", function (data) {
+        new Chart($('canvas#skills-coverage'), {
+          type   : 'radar',
+          data   : data,
+          options: {
+            scale : {
+              ticks: {
+                beginAtZero: true,
+                max        : 100
+              }
+            },
+            legend: {
+              display: false
+            }
+          }
+        });
+      });
+  </script>
+  
 @endpush
