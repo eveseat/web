eab5e0d823bc9ec98a5525c7166a699d08c371f3
Leon Jacobs <leonja511@gmail.com>
2018-05-27 17:53:48 +0200

    v3.0.0-beta18

--- src/Config/web.config.php
+++ src/Config/web.config.php
@@ -22,7 +22,7 @@
 
 return [
 
-    'version'                  => '3.0.0-beta17',
+    'version'                  => '3.0.0-beta18',
     'queue_status_update_time' => 10 * 1000, // milliseconds = seconds x 1,000
 
 ];
