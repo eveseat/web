f6cd5aa2dbe9e67f1ebd32435c622a3ed8277764
Leon Jacobs <leonja511@gmail.com>
2018-05-30 00:04:43 +0200

    v3.0.0-beta20

--- src/Config/web.config.php
+++ src/Config/web.config.php
@@ -22,7 +22,7 @@
 
 return [
 
-    'version'                  => '3.0.0-beta19',
+    'version'                  => '3.0.0-beta20',
     'queue_status_update_time' => 10 * 1000, // milliseconds = seconds x 1,000
 
 ];
