0171c24bd8ac81faeee321dd7562e3186ecf549e
Leon Jacobs <leonja511@gmail.com>
2018-06-14 17:06:47 +0200

    Remove unused variable and add refactor warning.

--- src/resources/views/configuration/users/list.blade.php
+++ src/resources/views/configuration/users/list.blade.php
@@ -1,5 +1,7 @@
 @extends('web::layouts.grids.12')
 
+{{-- #TODO Refactor this in 3.1, please. --}}
+
 @section('title', trans('web::seat.user_management'))
 @section('page_header', trans('web::seat.user_management'))
 
@@ -29,7 +31,7 @@
 
         <tbody>
 
-        @foreach($groups->sortBy(function($group, $key) { return strtolower(optional($group->main_character)->name); }) as $group)
+        @foreach($groups->sortBy(function($group) { return strtolower(optional($group->main_character)->name); }) as $group)
 
           <tr>
             <td rowspan="{{ $group->users->count() }}" style="border-top-width: 2px">
@@ -49,57 +51,57 @@
             </td>
             {{-- get first user attached to the group in order to end initial group seeding --}}
             <td style="border-top-width: 2px">
-              {!! img('character', $group->users->sortBy(function($user, $key) { return strtolower($user->name); })->first()->id, 64, ['class' => 'img-circle eve-icon small-icon', 'alt' => $group->users->sortBy(function($user, $key) { return strtolower($user->name); })->first()->name]) !!}
-              {{ $group->users->sortBy(function($user, $key) { return strtolower($user->name); })->first()->name }}
+              {!! img('character', $group->users->sortBy(function($user) { return strtolower($user->name); })->first()->id, 64, ['class' => 'img-circle eve-icon small-icon', 'alt' => $group->users->sortBy(function($user) { return strtolower($user->name); })->first()->name]) !!}
+              {{ $group->users->sortBy(function($user) { return strtolower($user->name); })->first()->name }}
             </td>
             <td style="border-top-width: 2px">
-              @if($group->users->sortBy(function($user, $key) { return strtolower($user->name); })->first()->active)
+              @if($group->users->sortBy(function($user) { return strtolower($user->name); })->first()->active)
                 <span class="label label-success">{{ trans('web::seat.enabled') }}</span>
               @else
                 <span class="label label-danger">{{ trans('web::seat.disabled') }}</span>
               @endif
             </td>
             <td style="border-top-width: 2px">
-              @if($group->users->sortBy(function($user, $key) { return strtolower($user->name); })->first()->refresh_token)
+              @if($group->users->sortBy(function($user) { return strtolower($user->name); })->first()->refresh_token)
                 <i class="fa fa-check text-success"></i>
               @else
                 <i class="fa fa-exclamation-triangle text-danger"></i>
               @endif
             </td>
             <td style="border-top-width: 2px">
-              <span data-toggle="tooltip" title="" data-original-title="{{ $group->users->sortBy(function($user, $key) { return strtolower($user->name); })->first()->last_login }}">
-                {{ human_diff($group->users->sortBy(function($user, $key) { return strtolower($user->name); })->first()->last_login) }}
+              <span data-toggle="tooltip" title="" data-original-title="{{ $group->users->sortBy(function($user) { return strtolower($user->name); })->first()->last_login }}">
+                {{ human_diff($group->users->sortBy(function($user) { return strtolower($user->name); })->first()->last_login) }}
               </span>
             </td>
-            <td style="border-top-width: 2px">{{ $group->users->sortBy(function($user, $key) { return strtolower($user->name); })->first()->last_login_source }}</td>
+            <td style="border-top-width: 2px">{{ $group->users->sortBy(function($user) { return strtolower($user->name); })->first()->last_login_source }}</td>
             <td style="border-top-width: 2px">
               <div class="btn-group">
-                <a href="{{ route('configuration.users.edit', ['user_id' => $group->users->sortBy(function($user, $key) { return strtolower($user->name); })->first()->id]) }}"
+                <a href="{{ route('configuration.users.edit', ['user_id' => $group->users->sortBy(function($user) { return strtolower($user->name); })->first()->id]) }}"
                    class="btn btn-warning btn-xs">
                   <i class="fa fa-pencil"></i>
                   {{ trans('web::seat.edit') }}
                 </a>
-                @if(auth()->user()->id != $group->users->sortBy(function($user, $key) { return strtolower($user->name); })->first()->id)
-                <a href="{{ route('configuration.users.impersonate', ['user_id' => $group->users->sortBy(function($user, $key) { return strtolower($user->name); })->first()->id]) }}"
+                @if(auth()->user()->id != $group->users->sortBy(function($user) { return strtolower($user->name); })->first()->id)
+                <a href="{{ route('configuration.users.impersonate', ['user_id' => $group->users->sortBy(function($user) { return strtolower($user->name); })->first()->id]) }}"
                    class="btn btn-default btn-xs">
                   <i class="fa fa-user-secret"></i>
                   {{ trans('web::seat.impersonate') }}
                 </a>
-                <a href="{{ route('configuration.users.delete', ['user_id' => $group->users->sortBy(function($user, $key) { return strtolower($user->name); })->first()->id]) }}"
+                <a href="{{ route('configuration.users.delete', ['user_id' => $group->users->sortBy(function($user) { return strtolower($user->name); })->first()->id]) }}"
                    class="btn btn-danger btn-xs confirmlink">
                   <i class="fa fa-trash"></i>
                   {{ trans('web::seat.delete') }}
                 </a>
                 @endif
               </div>
-              @if(auth()->user()->id == $group->users->sortBy(function($user, $key) { return strtolower($user->name); })->first()->id)
+              @if(auth()->user()->id == $group->users->sortBy(function($user) { return strtolower($user->name); })->first()->id)
                 <em class="text-danger">(This is you!)</em>
               @endif
             </td>
           </tr>
 
           {{-- get remaining users attached to the group in order to complete complexe row building --}}
-          @foreach($group->users->sortBy(function($user, $key) { return strtolower($user->name); })->slice(1) as $user)
+          @foreach($group->users->sortBy(function($user) { return strtolower($user->name); })->slice(1) as $user)
           <tr>
             <td>
               {!! img('character', $user->id, 64, ['class' => 'img-circle eve-icon small-icon', 'alt' => $user->name]) !!}
