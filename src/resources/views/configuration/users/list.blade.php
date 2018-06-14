fd223324d8fb687262e8fadaf76e337cd2d59f32
Loïc LEUILLIOT <e.elfaus@gmail.com>
2018-06-14 13:04:39 +0200

    fix user layout

--- src/resources/views/configuration/users/list.blade.php
+++ src/resources/views/configuration/users/list.blade.php
@@ -13,25 +13,30 @@
     </div>
     <div class="panel-body">
 
-      <table class="table datatable compact table-condensed table-hover table-responsive">
+      <table class="table compact table-condensed table-hover table-responsive">
         <thead>
-        <tr>
-          <th>{{ trans('web::seat.main_character') }}</th>
-          <th>{{ trans_choice('web::seat.role', 2) }}</th>
-          <th>{{ trans_choice('web::seat.group', 2) }}</th>
-        </tr>
+          <tr>
+            <th>{{ trans('web::seat.main_character') }}</th>
+            <th>{{ trans_choice('web::seat.role', 2) }}</th>
+            <th>{{ trans_choice('web::seat.name', 1) }}</th>
+            <th>{{ trans('web::seat.status') }}</th>
+            <th>{{ trans('web::seat.token') }}</th>
+            <th>{{ trans('web::seat.last_login') }}</th>
+            <th>{{ trans('web::seat.from') }}</th>
+            <th></th>
+          </tr>
         </thead>
 
         <tbody>
 
-        @foreach($groups as $group)
+        @foreach($groups->sortBy(function($group, $key) { return strtolower(optional($group->main_character)->name); }) as $group)
 
           <tr>
-            <td>
-              {!! img('character', optional($group->main_character)->character_id, 64, ['class' => 'img-circle eve-icon small-icon']) !!}
+            <td rowspan="{{ $group->users->count() }}" style="border-top-width: 2px">
+              {!! img('character', optional($group->main_character)->character_id, 64, ['class' => 'img-circle eve-icon small-icon', 'alt' => optional($group->main_character)->name]) !!}
               {{ optional($group->main_character)->name }}
             </td>
-            <td>
+            <td rowspan="{{ $group->users->count() }}" style="border-top-width: 2px">
               @if(count($group->roles) > 0)
                 <ul class="list-unstyled">
                   @foreach($group->roles as $role)
@@ -42,79 +47,113 @@
                 No Roles
               @endif
             </td>
-            <td>
-
-              <table class="table compact table-condensed table-hover table-responsive">
-                <thead>
-                <tr>
-                  <th>{{ trans_choice('web::seat.name', 1) }}</th>
-                  <th>{{ trans('web::seat.status') }}</th>
-                  <th>{{ trans('web::seat.token') }}</th>
-                  <th>{{ trans('web::seat.last_login') }}</th>
-                  <th>{{ trans('web::seat.from') }}</th>
-                  <th></th>
-                </tr>
-                </thead>
-
-                <tbody>
-                @foreach($group->users as $user)
-                  <tr>
-                    <td>
-                      {!! img('character', $user->id, 64, ['class' => 'img-circle eve-icon small-icon']) !!}
-                      {{ $user->name }}
-                    </td>
-                    <td>
-                      @if($user->active)
-                        <span class="label label-success">{{ trans('web::seat.enabled') }}</span>
-                      @else
-                        <span class="label label-danger">{{ trans('web::seat.disabled') }}</span>
-                      @endif
-                    </td>
-                    <td data-order="{{ $user->refresh_token ? 1 : 0 }}">
-                      @if($user->refresh_token)
-                        <i class="fa fa-check text-success"></i>
-                      @else
-                        <i class="fa fa-exclamation-triangle text-danger"></i>
-                      @endif
-                    </td>
-                    <td data-order="{{ $user->last_login }}">
-                      <span data-toggle="tooltip" title="" data-original-title="{{ $user->last_login }}">
-                        {{ human_diff($user->last_login) }}
-                      </span>
-                    </td>
-                    <td>{{ $user->last_login_source }}</td>
-                    <td>
-                      <div class="btn-group">
-                        <a href="{{ route('configuration.users.edit', ['user_id' => $user->id]) }}" type="button"
-                           class="btn btn-warning btn-xs">
-                          {{ trans('web::seat.edit') }}
-                        </a>
-                      </div>
-
-                      @if(auth()->user()->id != $user->id)
-                        <div class="btn-group">
-                          <a href="{{ route('configuration.users.delete', ['user_id' => $user->id]) }}" type="button"
-                             class="btn btn-danger btn-xs confirmlink">
-                            {{ trans('web::seat.delete') }}
-                          </a>
-                          <a href="{{ route('configuration.users.impersonate', ['user_id' => $user->id]) }}" type="button"
-                             class="btn btn-success btn-xs">
-                            {{ trans('web::seat.impersonate') }}
-                          </a>
-                        </div>
-                      @else
-                        <em class="text-danger">(This is you!)</em>
-                      @endif
-                    </td>
-                  </tr>
-                @endforeach
-                </tbody>
-              </table>
-
+            {{-- get first user attached to the group in order to end initial group seeding --}}
+            <td style="border-top-width: 2px">
+              {!! img('character', $group->users->sortBy(function($user, $key) { return strtolower($user->name); })->first()->id, 64, ['class' => 'img-circle eve-icon small-icon', 'alt' => $group->users->sortBy(function($user, $key) { return strtolower($user->name); })->first()->name]) !!}
+              {{ $group->users->sortBy(function($user, $key) { return strtolower($user->name); })->first()->name }}
+            </td>
+            <td style="border-top-width: 2px">
+              @if($group->users->sortBy(function($user, $key) { return strtolower($user->name); })->first()->active)
+                <span class="label label-success">{{ trans('web::seat.enabled') }}</span>
+              @else
+                <span class="label label-danger">{{ trans('web::seat.disabled') }}</span>
+              @endif
+            </td>
+            <td style="border-top-width: 2px">
+              @if($group->users->sortBy(function($user, $key) { return strtolower($user->name); })->first()->refresh_token)
+                <i class="fa fa-check text-success"></i>
+              @else
+                <i class="fa fa-exclamation-triangle text-danger"></i>
+              @endif
+            </td>
+            <td style="border-top-width: 2px">
+              <span data-toggle="tooltip" title="" data-original-title="{{ $group->users->sortBy(function($user, $key) { return strtolower($user->name); })->first()->last_login }}">
+                {{ human_diff($group->users->sortBy(function($user, $key) { return strtolower($user->name); })->first()->last_login) }}
+              </span>
+            </td>
+            <td style="border-top-width: 2px">{{ $group->users->sortBy(function($user, $key) { return strtolower($user->name); })->first()->last_login_source }}</td>
+            <td style="border-top-width: 2px">
+              <div class="btn-group">
+                <a href="{{ route('configuration.users.edit', ['user_id' => $group->users->sortBy(function($user, $key) { return strtolower($user->name); })->first()->id]) }}"
+                   class="btn btn-warning btn-xs">
+                  <i class="fa fa-pencil"></i>
+                  {{ trans('web::seat.edit') }}
+                </a>
+                @if(auth()->user()->id != $group->users->sortBy(function($user, $key) { return strtolower($user->name); })->first()->id)
+                <a href="{{ route('configuration.users.impersonate', ['user_id' => $group->users->sortBy(function($user, $key) { return strtolower($user->name); })->first()->id]) }}"
+                   class="btn btn-default btn-xs">
+                  <i class="fa fa-user-secret"></i>
+                  {{ trans('web::seat.impersonate') }}
+                </a>
+                <a href="{{ route('configuration.users.delete', ['user_id' => $group->users->sortBy(function($user, $key) { return strtolower($user->name); })->first()->id]) }}"
+                   class="btn btn-danger btn-xs confirmlink">
+                  <i class="fa fa-trash"></i>
+                  {{ trans('web::seat.delete') }}
+                </a>
+                @endif
+              </div>
+              @if(auth()->user()->id == $group->users->sortBy(function($user, $key) { return strtolower($user->name); })->first()->id)
+                <em class="text-danger">(This is you!)</em>
+              @endif
             </td>
           </tr>
 
+          {{-- get remaining users attached to the group in order to complete complexe row building --}}
+          @foreach($group->users->sortBy(function($user, $key) { return strtolower($user->name); })->slice(1) as $user)
+          <tr>
+            <td>
+              {!! img('character', $user->id, 64, ['class' => 'img-circle eve-icon small-icon', 'alt' => $user->name]) !!}
+              {{ $user->name }}
+            </td>
+            <td>
+              @if($user->active)
+                <span class="label label-success">{{ trans('web::seat.enabled') }}</span>
+              @else
+                <span class="label label-danger">{{ trans('web::seat.disabled') }}</span>
+              @endif
+            </td>
+            <td>
+              @if($user->refresh_token)
+                <i class="fa fa-check text-success"></i>
+              @else
+                <i class="fa fa-exclamation-triangle text-danger"></i>
+              @endif
+            </td>
+            <td>
+                      <span data-toggle="tooltip" title="" data-original-title="{{ $user->last_login }}">
+                        {{ human_diff($user->last_login) }}
+                      </span>
+            </td>
+            <td>{{ $user->last_login_source }}</td>
+            <td>
+              <div class="btn-group">
+                <a href="{{ route('configuration.users.edit', ['user_id' => $user->id]) }}"
+                   class="btn btn-warning btn-xs">
+                  <i class="fa fa-pencil"></i>
+                  {{ trans('web::seat.edit') }}
+                </a>
+                @if(auth()->user()->id != $user->id)
+                <a href="{{ route('configuration.users.impersonate', ['user_id' => $user->id]) }}"
+                   class="btn btn-default btn-xs">
+                  <i class="fa fa-user-secret"></i>
+                  {{ trans('web::seat.impersonate') }}
+                </a>
+                <a href="{{ route('configuration.users.delete', ['user_id' => $user->id]) }}"
+                   class="btn btn-danger btn-xs confirmlink">
+                  <i class="fa fa-trash"></i>
+                  {{ trans('web::seat.delete') }}
+                </a>
+                @endif
+              </div>
+              @if(auth()->user()->id == $user->id)
+                <em class="text-danger">(This is you!)</em>
+              @endif
+            </td>
+          </tr>
+          @endforeach
+
         @endforeach
+
         </tbody>
       </table>
 
