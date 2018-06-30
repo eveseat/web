2f033cc968f91fa58675b8c13b70bb5838bccf7b
mmolitor87 <molitor.michael@gmail.com>
2018-06-30 02:38:13 -0500

    Update ScheduleController.php (#190)

--- src/Http/Controllers/Configuration/ScheduleController.php
+++ src/Http/Controllers/Configuration/ScheduleController.php
@@ -50,7 +50,7 @@ class ScheduleController extends Controller
             'every minute'         => '* * * * *',
             'every five minutes'   => '*/5 * * * *',
             'every ten minutes'    => '*/10 * * * *',
-            'every thirty minutes' => '0,30 * * * *',
+            'every thirty minutes' => '0/30 * * * *',
 
         ];
 
