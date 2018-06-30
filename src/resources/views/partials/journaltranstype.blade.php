dbdb06bed0c4e3312db061ff995f8316dba96d3d
Leon Jacobs <leonja511@gmail.com>
2018-06-30 11:01:49 +0200

    Include the reason if available in wallet journal.

--- src/resources/views/partials/journaltranstype.blade.php
+++ src/resources/views/partials/journaltranstype.blade.php
@@ -4,3 +4,8 @@
      title="" data-original-title="{{ $row->description }}">
   </i>
 @endif
+@if(!is_null($row->reason))
+  <i class="fa fa-comment pull-right" data-toggle="tooltip"
+     title="" data-original-title="{{ $row->reason }}">
+  </i>
+@endif
