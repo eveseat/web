070696e3f74902084c89bb362bc45a02e4229b97
Herpaderp Aldent <huberfel@students.zhaw.ch>
2018-08-12 20:33:40 +0200

    Add tab for users with `character.list` permission * This regards #359 feature request * with SeAT Group Tab one can receive another table with only  character belonging to the users group.

--- src/resources/views/character/partials/alliancename.blade.php
+++ src/resources/views/character/partials/alliancename.blade.php
@@ -1,4 +1,4 @@
 @if(! is_null($row->alliance_id))
-  {!! img('alliance', $row->alliance_id, 64, ['class' => 'img-circle eve-icon small-icon']) !!}
+  {!! img('alliance', $row->alliance_id, 64, ['class' => 'img-circle eve-icon small-icon'], false) !!}
   <span rel="id-to-name">{{ $row->alliance_id }}</span>
 @endif
