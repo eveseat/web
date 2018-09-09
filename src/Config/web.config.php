8f49b5d7e8e19684b2791a405b09a1c66647d1c4
Leon Jacobs <leonja511@gmail.com>
2018-09-09 11:15:53 +0200

    v3.0.7

--- src/Config/web.config.php
+++ src/Config/web.config.php
@@ -22,7 +22,7 @@
 
 return [
 
-    'version'                  => '3.0.6',
+    'version'                  => '3.0.7',
     'queue_status_update_time' => 10 * 1000, // milliseconds = seconds x 1,000
 
 ];
