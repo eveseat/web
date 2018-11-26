c4ed40fde82c359f8ae01a07cc678b338e4d62d9
Herpaderp Aldent <felix.a.huber@gmx.net>
2018-11-26 13:24:04 +0100

    Some style fix

--- src/resources/views/partials/killmailsystem.blade.php
+++ src/resources/views/partials/killmailsystem.blade.php
@@ -1,12 +1,12 @@
 {{ $place->itemName }}
 
 <span class="
-  @if($place->security >= 0.5)
+  @if ($place->security >= 0.5)
     text-green
-@elseif($place->security < 0.5 && $place->security > 0.0)
+@elseif ($place->security < 0.5 && $place->security > 0.0)
     text-warning
 @else
     text-red
 @endif">
-  <i>({{ round($place->security,  2) }})</i>
+  <i>({{ round($place->security, 2) }})</i>
 </span>
