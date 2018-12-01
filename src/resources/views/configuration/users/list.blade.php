6a44a3b728285d2a3247838f083de37a0a8a5d75
Herpaderp Aldent <felix.a.huber@gmx.net>
2018-12-01 08:39:31 +0100

    move helper function to `seat.js`

--- src/resources/views/configuration/users/list.blade.php
+++ src/resources/views/configuration/users/list.blade.php
@@ -72,19 +72,6 @@
       },
     });
 
-    function wordwrap( str, width, brk, cut ) {
-
-      brk = brk || 'n';
-      width = width || 75;
-      cut = cut || false;
-
-      if (!str) { return str; }
-
-      var regex = '.{1,' +width+ '}(\s|$)' + (cut ? '|.{' +width+ '}|.+$' : '|\S+?(\s|$)');
-
-      return str.match( RegExp(regex, 'g') ).join( brk );
-
-    }
   </script>
 
 @endpush
