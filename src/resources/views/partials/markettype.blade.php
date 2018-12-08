7864e9c9bc3da94d71585ba5fc18708175fa9777
Herpaderp Aldent <felix.a.huber@gmx.net>
2018-12-08 15:53:23 +0100

    [feat](market): Update market tab and refactor partials (#240)
    * Update market tab and refactor partials

--- src/resources/views/partials/markettype.blade.php
+++ src/resources/views/partials/markettype.blade.php
@@ -2,5 +2,8 @@
       title="" data-original-title="{{ $row->stationName }}">
   <i class="fa fa-map-marker"></i>
 </span>
+@if (request('all_linked_characters') === "true")
+  {!! img('character', $row->character_id, 32, ['class' => 'img-circle eve-icon small-icon'], false) !!}
+@endif
 {!! img('type', $row->type_id, 64, ['class' => 'img-circle eve-icon small-icon']) !!}
 {{ $row->typeName }}
