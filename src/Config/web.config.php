679d370d318514e52170d52b27d942cbdce9391b
Loïc LEUILLIOT <loic.leuilliot@gmail.com>
2018-12-10 19:33:10 +0100

    build(core): v3.0.11

--- src/Config/web.config.php
+++ src/Config/web.config.php
@@ -22,7 +22,7 @@
 
 return [
 
-    'version'                  => '3.0.10',
+    'version'                  => '3.0.11',
     'queue_status_update_time' => 10 * 1000, // milliseconds = seconds x 1,000
 
 ];
