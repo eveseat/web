5689947d6544813f1b1dbe9e38e8fa48b21935d9
Herpaderp Aldent <felix.a.huber@gmx.net>
2018-12-17 21:02:36 +0000

    Apply fixes from StyleCI

--- src/Acl/AccessChecker.php
+++ src/Acl/AccessChecker.php
@@ -304,7 +304,7 @@ trait AccessChecker
             // mind here that affiliations can have inversions too.
             foreach ($role->affiliations as $affiliation) {
 
-                if ($affiliation->pivot->not && !in_array($affiliation->affiliation, $this->associatedCharacterIds()->toArray())) {
+                if ($affiliation->pivot->not && ! in_array($affiliation->affiliation, $this->associatedCharacterIds()->toArray())) {
 
                     array_push(
                         $map['inverted_affiliations'][$affiliation->type],
