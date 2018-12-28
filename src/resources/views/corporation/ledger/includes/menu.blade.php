c1e802b9b780c7df0290dcd8690511623484fa18
Joel Falknau <ozirascal@gmail.com>
2018-12-28 18:12:53 +1000

    add navigation buttons for new views

--- src/resources/views/corporation/ledger/includes/menu.blade.php
+++ src/resources/views/corporation/ledger/includes/menu.blade.php
@@ -16,6 +16,32 @@
         {{ trans_choice('web::seat.pi', 2) }}
       </a>
     </li>
+    <li role="presentation" class="@if ($sub_viewname == 'industryfacilitytaxbymonth') active @endif">
+      <a href="{{ route('corporation.view.ledger.industryfacilitytaxbymonth', $sheet->corporation_id) }}">
+        {{ trans_choice('web::seat.industryfacilitytaxbymonth', 2) }}
+      </a>
+    </li>
+    <li role="presentation" class="@if ($sub_viewname == 'jumpbridgebymonth') active @endif">
+      <a href="{{ route('corporation.view.ledger.jumpbridgebymonth', $sheet->corporation_id) }}">
+        {{ trans_choice('web::seat.jumpbridgebymonth', 2) }}
+      </a>
+    </li>
+    <li role="presentation" class="@if ($sub_viewname == 'jumpclonebymonth') active @endif">
+      <a href="{{ route('corporation.view.ledger.jumpclonebymonth', $sheet->corporation_id) }}">
+        {{ trans_choice('web::seat.jumpclonebymonth', 2) }}
+      </a>
+    </li>
+    <li role="presentation" class="@if ($sub_viewname == 'officerentalfeebymonth') active @endif">
+      <a href="{{ route('corporation.view.ledger.officerentalfeebymonth', $sheet->corporation_id) }}">
+        {{ trans_choice('web::seat.officerentalfeebymonth', 2) }}
+      </a>
+    </li>
+
+    <li role="presentation" class="@if ($sub_viewname == 'reprocessingfeebymonth') active @endif">
+      <a href="{{ route('corporation.view.ledger.reprocessingfeebymonth', $sheet->corporation_id) }}">
+        {{ trans_choice('web::seat.reprocessingfeebymonth', 2) }}
+      </a>
+    </li>
   </ul>
 
 </div>
