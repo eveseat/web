32465454ea38a18fb17539e5ce4b85a2901525a1
Herpaderp Aldent <felix.a.huber@gmx.net>
2019-01-06 12:14:19 +0100

    feat(tracking): Move corporation tracking table rendering to server
    * Fix corporation tracking and move to server side datatables
    
    * Apply fixes from StyleCI
    
    * Incorporate some of warlofs feedback:
    Reducing complexity in submitted parameters using array
    
    * Safe my ass from an exception due to missing user, group OR unset mainchar
    
    * encapsulate labels in a div-group
    
    * better prevent null-object exception
    
    * testing the online editor

--- src/resources/views/corporation/tracking.blade.php
+++ src/resources/views/corporation/tracking.blade.php
@@ -3,83 +3,92 @@
 @section('title', trans_choice('web::seat.corporation', 1) . ' ' . trans('web::seat.tracking'))
 @section('page_header', trans_choice('web::seat.corporation', 1) . ' ' . trans('web::seat.tracking'))
 
+@inject('request', 'Illuminate\Http\Request')
+
 @section('corporation_content')
 
-  <div class="panel panel-default">
-    <div class="panel-heading">
-      <h3 class="panel-title">{{ trans('web::seat.tracking') }}</h3>
+  <div class="box box-default">
+    <div class="box-header with-border">
+      <h3 class="box-title">{{ trans('web::seat.tracking') }}</h3>
     </div>
-    <div class="panel-body">
+    <div class="box-body">
 
-      <table class="table datatable compact table-condensed table-hover table-responsive">
+      <div>
+        <b>{{ trans('web::seat.status') }}</b>
+        <div class="input-group">
+          <label class="checkbox-inline">
+            <input onchange="filterme()" type="checkbox" name="token_status" value="valid_token" checked>{{ trans('web::seat.valid_token') }}
+          </label>
+          <label class="checkbox-inline">
+            <input onchange="filterme()" type="checkbox" name="token_status" value="invalid_token" checked>{{ trans('web::seat.invalid_token') }}
+          </label>
+        </div>
+      </div>
+
+      <table id="corporation-member-tracking" class="table compact table-condensed table-hover table-responsive">
         <thead>
           <tr>
-            <th>{{ trans('web::seat.token') }}</th>
+            <th data-orderable="false">{{ trans('web::seat.token') }}</th>
             <th>{{ trans_choice('web::seat.name', 1) }}</th>
-            <th>{{trans('web::seat.group_main')}}</th>
             <th>{{ trans('web::seat.last_location') }}</th>
             <th>{{ trans('web::seat.joined') }}</th>
             <th>{{ trans('web::seat.last_login') }}</th>
           </tr>
         </thead>
-        <tbody>
-
-        @foreach($tracking as $character)
-
-            <tr>
-              <td data-order="{{ $character->key_ok }}">
-                @if($character->key_ok == 1)
-                  <i class="fa fa-check text-success"></i>
-                @else
-                  <i class="fa fa-exclamation-triangle text-danger"></i>
-                @endif
-              </td>
-              <td>
-                <a href="{{ route('character.view.sheet', ['character_id' => $character->character_id]) }}">
-                  {!! img('character', $character->character_id, 64, ['class' => 'img-circle eve-icon small-icon']) !!}
-                  <span class="id-to-name"
-                        data-id="{{$character->character_id}}">{{ trans('web::seat.unknown') }}</span>
-                </a>
-              </td>
-              <td>
-                <span class="character-id-to-main-character"
-                      data-character-id="{{$character->character_id}}">{{ trans('web::seat.unknown') }}</span>
-                @if(!is_null($character->ship_type_id))
-                  <i class="pull-right" data-toggle="tooltip" title="" data-original-title="{{ $character->type->typeName }}">
-                    {!! img('type', $character->ship_type_id, 64, ['class' => 'img-circle eve-icon small-icon']) !!}
-                  </i>
-                @endif
-              </td>
-              <td>
-                @if(! is_null($character->location))
-                {{ $character->location->name }}
-                @else
-                Unknown Location
-                @endif
-              </td>
-              <td data-order="{{ $character->start_date }}">
-                <span data-toggle="tooltip" title="" data-original-title="{{ $character->start_date }}">
-                  {{ human_diff($character->start_date) }}
-                </span>
-              </td>
-              <td data-order="{{ $character->logon_date }}">
-                <span data-toggle="tooltip" title="" data-original-title="{{ $character->logon_date }}">
-                  {{ human_diff($character->logon_date) }}
-                </span>
-              </td>
-            </tr>
-
-        @endforeach
-
-        </tbody>
       </table>
 
     </div>
-    <div class="panel-footer">Registered users <b>{{ $tracking->where('key_ok', true)->count() }} / {{ $tracking->count() }}</b></div>
   </div>
 
 @stop
 
 @push('javascript')
-  @include('web::includes.javascript.character-id-to-main-character')
+
+  <script>
+
+    function refreshTokenCheckboxes() {
+      return $('input:checkbox[name="token_status"]:checked').map(function() {
+        return  this.value;
+      }).get();
+    }
+
+    function filterme() {
+
+      corporation_member_tracking_table.draw()
+    }
+
+    var corporation_member_tracking_table = $('table#corporation-member-tracking').DataTable({
+      processing      : true,
+      serverSide      : true,
+      ajax            : {
+        url : '{{ route('corporation.view.tracking.data', ['corporation_id' => $request->corporation_id]) }}',
+        data: function ( d ) {
+          d.selected_refresh_token_status = refreshTokenCheckboxes();
+        }
+      },
+      columns         : [
+        {data: 'refresh_token', name: 'user.refresh_token', orderable: false, searchable: false},
+        {data: 'character_id', name: 'user.name'},
+        {data: 'location', name: 'location', searchable: false},
+        {data: 'start_date', name: 'start_date', render: human_readable, searchable: false},
+        {data: 'logon_date', name: 'logon_date', render: human_readable, searchable: false}
+      ],
+      rowGroup: {
+        startRender: function(rows, group) {
+
+          var character_group = rows.data().pluck('main_character')[0];
+
+          return '{{trans('web::seat.main_character')}}: ' + character_group;
+        },
+        dataSrc: 'main_character'
+      },
+      drawCallback: function () {
+        $("img").unveil(100);
+        ids_to_names();
+        $('[data-toggle="tooltip"]').tooltip();
+      },
+    });
+
+  </script>
+
 @endpush
