c986a69178e6a24bb7b16ac552c08bdc012180cb
Loïc Leuilliot <e.elfaus@gmail.com>
2019-01-06 12:20:51 +0100

    feat(mining): Reduce numbers precisions (#260)
    Remove the decimals from mined quantity and reduce the volume precision
    from 2 to 1 decimal.
    
    Closes eveseat/seat#487

--- src/resources/views/character/mining-ledger.blade.php
+++ src/resources/views/character/mining-ledger.blade.php
@@ -50,8 +50,8 @@
               {!! img('type', $entry->type->typeID, 32, ['class' => 'img-circle eve-icon small-icon']) !!}
               {{ $entry->type->typeName }}
             </td>
-            <td class="text-right" data-order="{{ $entry->quantity }}">{{ number($entry->quantity) }}</td>
-            <td class="text-right" data-order="{{ $entry->volumes }}">{{ number($entry->volumes) }} m3</td>
+            <td class="text-right" data-order="{{ $entry->quantity }}">{{ number($entry->quantity, 0) }}</td>
+            <td class="text-right" data-order="{{ $entry->volumes }}">{{ number($entry->volumes, 1) }} m3</td>
             <td class="text-right" data-order="{{ $entry->amounts }}">{{ number($entry->amounts) }} ISK
               <a href="#" class="btn btn-sm btn-link" data-toggle="modal" data-target="#detailed-ledger">
                 <i class="fa fa-cubes"></i>
@@ -111,7 +111,6 @@
                 {data: 'amounts'}
               ]
             });
-
 
           })
           .on('hidden.bs.modal', function (e) {
