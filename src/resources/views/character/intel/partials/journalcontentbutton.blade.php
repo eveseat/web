6ae50e033dbf7e97fe4391d6679972e88d3f9156
Herpaderp Aldent <felix.a.huber@gmx.net>
2019-01-07 21:40:03 +0100

    feat(character): Move intel to asynchronous requests and improve UI
    * Intel-Wallet WIP
    * Working UniverseName function
    * Dispatch name and affiliation job upon access to intel-tab
    * Disable sorting and searching
    * WIP ....
    * Highlight inner_group_trades
    * Working new intel datatables
    * Detail Modals introduced
    * Top Wallet Journal
    * Top Wallet Transactions
    * Detail Modals introduced
    * Top Mail Interactions
    * Full intel controller
    * Add inner group wallet highlight
    * use search-plus favicon and refactor button blade names
    * Rearrange columns to fix UI bug of missing th
    * Review feedback implemented

---
+++ src/resources/views/character/intel/partials/journalcontentbutton.blade.php
@@ -0,0 +1,6 @@
+<span class="pull-right">
+  <a href="#" class="journal-content" data-toggle="modal" data-target="#journalContentModal"
+     data-url="{{ route('character.view.intel.summary.journal.details', ['first_party_id' => $row->first_party_id, 'second_party_id' => $row->second_party_id]) }}">
+    <i class="fa fa-search-plus"></i>
+  </a>
+</span>
