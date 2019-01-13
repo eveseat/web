077df2a4fc59cf6f8575cd896ea8615612b2cb10
Loïc LEUILLIOT <loic.leuilliot@gmail.com>
2019-01-13 11:55:33 +0100

    build(core): v3.0.14

--- src/Config/web.config.php
+++ src/Config/web.config.php
@@ -22,7 +22,7 @@
 
 return [
 
-    'version'                  => '3.0.13',
+    'version'                  => '3.0.14',
     'queue_status_update_time' => 10 * 1000, // milliseconds = seconds x 1,000
 
 ];
