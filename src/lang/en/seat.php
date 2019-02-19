543f8d8b8e0640802f3dadc105371176f91b7d1b
Herpaderp Aldent <felix.a.huber@gmx.net>
2019-02-19 01:53:02 +0100

    Add warning about outdated information. (#273)
    * Add warning about outdated information.
    
    * update text
    
    Update text for warning according to qu1ckkk's inout

--- src/lang/en/seat.php
+++ src/lang/en/seat.php
@@ -522,7 +522,7 @@ return [
     'client'                       => 'Client',
     'qty'                          => '#',
     'unknown_character'            => 'Unknown Character. Is the API Key on record and updated?',
-    'no_account_info'              => 'No Account Information Available',
+    'deleted_refresh_token'        => 'A refresh token for this character was deleted about :time. As a result, information displayed may be out of date.',
     'channel_members'              => 'Channel Members',
     'motd'                         => 'MoTD',
     'bookmark'                     => 'Bookmark|Bookmarks',
