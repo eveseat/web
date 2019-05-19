e21361ac7639dc03410dcb57f5442778246afd82
Loïc Leuilliot <loic.leuilliot@gmail.com>
2019-05-19 14:54:59 +0200

    fix(config): put environment variables into config file (#304)
    parameters from .env is only read at boot stage - then, configuration
    files are used.
    
    https://github.com/laravel/framework/issues/21727

--- src/Http/Controllers/Configuration/SeatController.php
+++ src/Http/Controllers/Configuration/SeatController.php
@@ -49,9 +49,9 @@ class SeatController extends Controller
         $packages = $this->getPluginsMetadataList();
 
         // Validate SSO Environment settings
-        if (is_null(env('EVE_CLIENT_ID')) or
-            is_null(env('EVE_CLIENT_SECRET')) or
-            is_null(env('EVE_CALLBACK_URL'))
+        if (is_null(config('eveapi.config.eseye_client_id')) or
+            is_null(config('eveapi.config.eseye_client_secret')) or
+            is_null(config('eveapi.config.eseye_client_callback'))
         )
             $warn_sso = true;
         else
