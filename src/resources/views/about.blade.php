7d834a4d257de99923556d203e6598847ddc98ed
Loïc LEUILLIOT <loic.leuilliot@gmail.com>
2019-06-05 18:44:15 +0200

    fix(license): add missing ESI/EVE Dev licence link and sort components

--- src/resources/views/about.blade.php
+++ src/resources/views/about.blade.php
@@ -16,12 +16,12 @@
         <div class="box-body">
           <p>SeAT is built upon a large panel of components which are published under the following licenses :</p>
           <ul>
-            <li>CCP ~ <a href="#">Third Party License Agreement</a></li>
             <li>jQuery ~ <a href="https://opensource.org/licenses/mit-license.html">MIT License</a></li>
+            <li>Laravel ~ <a href="https://opensource.org/licenses/mit-license.html">MIT License</a></li>
             <li>Admin LTE ~ <a href="https://opensource.org/licenses/mit-license.html">MIT License</a></li>
             <li>Datatables ~ <a href="https://opensource.org/licenses/mit-license.html">MIT License</a></li>
-            <li>Laravel ~ <a href="https://opensource.org/licenses/mit-license.html">MIT License</a></li>
             <li>Fontawesome ~ <a href="https://opensource.org/licenses/mit-license.html">MIT License</a></li>
+            <li>ESI & EVE Online assets ~ <a href="https://developers.eveonline.com/resource/license-agreement">Third Party License Agreement</a></li>
           </ul>
           <p>SeAT himself is published under the <a href="https://opensource.org/licenses/GPL-2.0">GNU General Public License (GPL)</a>.</p>
         </div>
