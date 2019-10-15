f29941a07a34646b0ea9140928323a3d5683ef15
Loïc LEUILLIOT <loic.leuilliot@gmail.com>
2019-10-15 19:16:15 +0200

    feat(deps): upgrade corporations wallet sub-menu

--- src/resources/views/corporation/wallet/includes/menu.blade.php
+++ src/resources/views/corporation/wallet/includes/menu.blade.php
@@ -2,13 +2,13 @@
 
   <ul class="nav nav-pills">
     @if(auth()->user()->has('corporation.journal'))
-    <li role="presentation" class="@if($sub_viewname == 'journal') active @endif">
-      <a href="{{ route('corporation.view.journal', $sheet->corporation_id) }}">Journal</a>
+    <li role="presentation" class="nav-item">
+      <a href="{{ route('corporation.view.journal', $sheet->corporation_id) }}" class="nav-link @if($sub_viewname == 'journal') active @endif">Journal</a>
     </li>
     @endif
     @if(auth()->user()->has('corporation.transactions'))
-    <li role="presentation" class="@if($sub_viewname == 'transactions') active @endif">
-      <a href="{{ route('corporation.view.transactions', $sheet->corporation_id) }}">Transactions</a>
+    <li role="presentation" class="nav-item">
+      <a href="{{ route('corporation.view.transactions', $sheet->corporation_id) }}" class="nav-link @if($sub_viewname == 'transactions') active @endif">Transactions</a>
     </li>
     @endif
   </ul>
