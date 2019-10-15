b37a45b93f0adfe3baceab682a3e33d7c86cd918
Loïc LEUILLIOT <loic.leuilliot@gmail.com>
2019-10-15 16:37:21 +0200

    feat(deps): upgrade character research

--- src/resources/views/character/research.blade.php
+++ src/resources/views/character/research.blade.php
@@ -6,22 +6,22 @@
 
 @section('character_content')
 
-  <div class="panel panel-default">
-    <div class="panel-heading">
-      <h3 class="panel-title">
+  <div class="card">
+    <div class="card-header">
+      <h3 class="card-title">
         {{ trans('web::seat.research') }}
-        @if(auth()->user()->has('character.jobs'))
-          <span class="pull-right">
-            <a href="{{ route('tools.jobs.dispatch', ['character_id' => $request->character_id, 'job_name' => 'character.research']) }}"
-               style="color: #000000">
-              <i class="fa fa-refresh" data-widget="tooltip" title="{{ trans('web::seat.update_research') }}"></i>
-            </a>
-          </span>
-        @endif
       </h3>
+      @if(auth()->user()->has('character.jobs'))
+        <div class="card-tools">
+          <a href="{{ route('tools.jobs.dispatch', ['character_id' => $request->character_id, 'job_name' => 'character.research']) }}"
+             class="pull-right" style="color: #000000">
+            <i class="fas fa-sync" data-widget="tooltip" title="{{ trans('web::seat.update_research') }}"></i>
+          </a>
+        </div>
+      @endif
     </div>
-    <div class="panel-body">
-      <div class="margin-bottom">
+    <div class="card-body">
+      <div class="mb-3">
         <select multiple="multiple" id="dt-character-selector" class="form-control">
           @foreach($characters as $character)
             @if($character->id == $request->character_id)
