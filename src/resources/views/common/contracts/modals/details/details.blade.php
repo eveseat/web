c323c1746987a40064104abe928f5b41f2d99c5e
Loïc LEUILLIOT <loic.leuilliot@gmail.com>
2019-10-15 18:04:28 +0200

    feat(deps): upgrade character contracts

--- src/resources/views/common/contracts/modals/details/details.blade.php
+++ src/resources/views/common/contracts/modals/details/details.blade.php
@@ -1,13 +1,12 @@
 <!-- Contracts Items Modal -->
-<div class="modal fade" id="contract-detail" tabindex="-1" role="dialog"
-     aria-labelledby="contract-detail-label">
-  <div class="modal-dialog" role="document">
+<div class="modal fade" id="contract-detail" tabindex="-1" role="dialog" aria-labelledby="contract-detail-label">
+  <div class="modal-dialog modal-lg modal-dialog-scrollable">
     <div class="modal-content">
       <div class="modal-header bg-blue">
+        <h4 class="modal-title" id="contract-detail-label">{{ trans('web::seat.contract_items') }}</h4>
         <button type="button" class="close" data-dismiss="modal" aria-label="Close">
           <span aria-hidden="true">&times;</span>
         </button>
-        <h4 class="modal-title" id="contract-detail-label">{{ trans('web::seat.contract_items') }}</h4>
       </div>
       <div class="modal-body"></div>
     </div>
