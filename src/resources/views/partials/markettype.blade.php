729ce4f9562b271a6f340d40471e3f20438d8be0
Loïc LEUILLIOT <loic.leuilliot@gmail.com>
2019-10-15 23:34:08 +0200

    fix(deps): use proper map mark icon for font-awesome 5

--- src/resources/views/partials/markettype.blade.php
+++ src/resources/views/partials/markettype.blade.php
@@ -1,6 +1,6 @@
 <span data-toggle="tooltip"
       title="" data-original-title="{{ $row->stationName }}">
-  <i class="fa fa-map-marker"></i>
+  <i class="fas fa-map-marker-alt"></i>
 </span>
 @if (request('all_linked_characters') === "true")
   {!! img('character', $row->character_id, 32, ['class' => 'img-circle eve-icon small-icon']) !!}
