16d4032192dec57e8a796b99a56e45cbfe78069c
Loïc LEUILLIOT <loic.leuilliot@gmail.com>
2019-10-15 16:35:55 +0200

    feat(deps): upgrade character wallet navigation

--- src/resources/views/character/wallet/includes/menu.blade.php
+++ src/resources/views/character/wallet/includes/menu.blade.php
@@ -1,14 +1,14 @@
-<div style="padding-bottom: 15px;">
+<div class="pb-3">
 
   <ul class="nav nav-pills">
     @if(auth()->user()->has('character.journal'))
-    <li role="presentation" class="@if ($sub_viewname == 'journal') active @endif">
-      <a href="{{ route('character.view.journal', $summary->character_id) }}">Journal</a>
+    <li role="presentation" class="nav-item">
+      <a href="{{ route('character.view.journal', $summary->character_id) }}" class="nav-link @if ($sub_viewname == 'journal') active @endif">Journal</a>
     </li>
     @endif
     @if(auth()->user()->has('character.transactions'))
-    <li role="presentation" class="@if($sub_viewname == 'transactions') active @endif">
-      <a href="{{ route('character.view.transactions', $summary->character_id) }}">Transactions</a>
+    <li role="presentation" class="nav-item">
+      <a href="{{ route('character.view.transactions', $summary->character_id) }}" class="nav-link @if($sub_viewname == 'transactions') active @endif">Transactions</a>
     </li>
     @endif
   </ul>
