5a47dbe9236d6a7d06b788315b1f04ee8c673d6d
Loïc LEUILLIOT <loic.leuilliot@gmail.com>
2019-10-15 16:46:00 +0200

    feat(deps): upgrade character planetary interaction

--- src/resources/views/character/pi.blade.php
+++ src/resources/views/character/pi.blade.php
@@ -6,53 +6,53 @@
 
 @section('character_content')
 
-  <div class="panel panel-default">
-    <div class="panel-heading">
-      <h3 class="panel-title">
+  <div class="card">
+    <div class="card-header">
+      <h3 class="card-title">
         {{ trans('web::seat.pi') }}
-        @if(auth()->user()->has('character.jobs'))
-          <span class="pull-right">
-            <a href="{{ route('tools.jobs.dispatch', ['character_id' => $request->character_id, 'job_name' => 'character.pi']) }}"
-               style="color: #000000">
-              <i class="fa fa-refresh" data-widget="tooltip" title="{{ trans('web::seat.update_pi') }}"></i>
-            </a>
-          </span>
-        @endif
       </h3>
+      @if(auth()->user()->has('character.jobs'))
+        <div class="card-tools">
+          <a href="{{ route('tools.jobs.dispatch', ['character_id' => $request->character_id, 'job_name' => 'character.pi']) }}"
+             class="float-right" style="color: #000000">
+            <i class="fas fa-sync" data-widget="tooltip" title="{{ trans('web::seat.update_pi') }}"></i>
+          </a>
+        </div>
+      @endif
     </div>
-    <div class="panel-body">
+    <div class="card-body">
 
-      <table class="table datatable compact table-condensed table-hover table-responsive">
+      <table class="table datatable table-sm table-condensed table-striped table-hover">
         <thead>
-        <tr>
-          <th>{{ trans('web::seat.updated') }}</th>
-          <th>{{ trans('web::seat.system') }}</th>
-          <th>{{ trans('web::seat.planet') }}</th>
-          <th>{{ trans('web::seat.upgrade_level') }}</th>
-          <th>{{ trans('web::seat.no_pins') }}</th>
-        </tr>
+          <tr>
+            <th>{{ trans('web::seat.updated') }}</th>
+            <th>{{ trans('web::seat.system') }}</th>
+            <th>{{ trans('web::seat.planet') }}</th>
+            <th>{{ trans('web::seat.upgrade_level') }}</th>
+            <th>{{ trans('web::seat.no_pins') }}</th>
+          </tr>
         </thead>
         <tbody>
 
-        @foreach($colonies as $colony)
+          @foreach($colonies as $colony)
 
-          <tr>
-            <td data-order="{{ $colony->last_update }}">
-              <span data-widget="tooltip"
-                    title="" data-original-title="{{ $colony->last_update }}">
-                {{ human_diff($colony->last_update) }}
-              </span>
-            </td>
-            <td>{{ $colony->itemName }}</td>
-            <td>
-              {!! img('type', $colony->typeID, 64, ['class' => 'img-circle eve-icon small-icon']) !!}
-              {{ ucfirst($colony->planet_type) }}
-            </td>
-            <td>{{ $colony->upgrade_level }}</td>
-            <td>{{ $colony->num_pins }}</td>
-          </tr>
+            <tr>
+              <td data-order="{{ $colony->last_update }}">
+                <span data-widget="tooltip"
+                      title="" data-original-title="{{ $colony->last_update }}">
+                  {{ human_diff($colony->last_update) }}
+                </span>
+              </td>
+              <td>{{ $colony->itemName }}</td>
+              <td>
+                {!! img('type', $colony->typeID, 64, ['class' => 'img-circle eve-icon small-icon']) !!}
+                {{ ucfirst($colony->planet_type) }}
+              </td>
+              <td>{{ $colony->upgrade_level }}</td>
+              <td>{{ $colony->num_pins }}</td>
+            </tr>
 
-        @endforeach
+          @endforeach
 
         </tbody>
       </table>
@@ -60,46 +60,46 @@
     </div>
   </div>
 
-  <div class="panel panel-default">
-    <div class="panel-heading">
-      <h3 class="panel-title">Extractors</h3>
+  <div class="card">
+    <div class="card-header">
+      <h3 class="card-title">Extractors</h3>
     </div>
-    <div class="panel-body">
+    <div class="card-body">
 
-      <table class="table datatable compact table-condensed table-hover table-responsive">
+      <table class="table datatable table-sm table-condensed table-striped table-hover">
         <thead>
-        <tr>
-          <th>{{ trans('web::seat.planet') }}</th>
-          <th>{{ trans('web::seat.system') }}</th>
-          <th>{{ trans('web::seat.product') }}</th>
-          <th>{{ trans('web::seat.progress') }}</th>
-          <th>{{ trans('web::seat.expiry') }}</th>
-        </tr>
+          <tr>
+            <th>{{ trans('web::seat.planet') }}</th>
+            <th>{{ trans('web::seat.system') }}</th>
+            <th>{{ trans('web::seat.product') }}</th>
+            <th>{{ trans('web::seat.progress') }}</th>
+            <th>{{ trans('web::seat.expiry') }}</th>
+          </tr>
         </thead>
         <tbody>
 
-        @foreach($extractors as $extractor)
+          @foreach($extractors as $extractor)
 
-          <tr>
-            <td>
-              {!! img('type', $extractor['typeID'], 64, ['class' => 'img-circle eve-icon small-icon']) !!}
-              {{ ucfirst($extractor['planet_type']) }}
-            </td>
-            <td>{{ $extractor['itemName'] . " " . $extractor['celestialIndex'] }}</td>
-            <td>
-              {!! img('type', $extractor['product_type_id'], 64, ['class' => 'img-circle eve-icon small-icon']) !!}
-              {{ ucfirst($extractor['typeName']) }}
-            </td>
-            <td>
-              <div class="countdown-progressbar" data-expiry-time="{{$extractor['expiry_time']}}"
-                   data-install-time="{{$extractor['install_time']}}"></div>
-            </td>
-            <td>
-              <div class="countdown" data-expiry-time="{{$extractor['expiry_time']}}">{{$extractor['expiry_time']}}</div>
-            </td>
-          </tr>
+            <tr>
+              <td>
+                {!! img('type', $extractor['typeID'], 64, ['class' => 'img-circle eve-icon small-icon']) !!}
+                {{ ucfirst($extractor['planet_type']) }}
+              </td>
+              <td>{{ $extractor['itemName'] . " " . $extractor['celestialIndex'] }}</td>
+              <td>
+                {!! img('type', $extractor['product_type_id'], 64, ['class' => 'img-circle eve-icon small-icon']) !!}
+                {{ ucfirst($extractor['typeName']) }}
+              </td>
+              <td>
+                <div class="countdown-progressbar" data-expiry-time="{{$extractor['expiry_time']}}"
+                     data-install-time="{{$extractor['install_time']}}"></div>
+              </td>
+              <td>
+                <div class="countdown" data-expiry-time="{{$extractor['expiry_time']}}">{{$extractor['expiry_time']}}</div>
+              </td>
+            </tr>
 
-        @endforeach
+          @endforeach
 
         </tbody>
       </table>
@@ -127,7 +127,7 @@
           var percentage_complete = (moment.utc() - install_time) / (expiry_time - install_time);
           var percentage_rounded = (Math.round(percentage_complete * 100) / 100);
           var progress_value = percentage_rounded * 100;
-          var progress_class = 'progress-bar';
+          var progress_class = 'progress-bar progress-bar-striped';
 
           // ensure progress-bar will not exceed 100%
           if (progress_value > 100)
@@ -148,7 +148,7 @@
                   progress_class += ' progress-bar-danger progress-bar-striped';
                   break;
               default:
-                  progress_class += ' progress-bar-danger';
+                  progress_class += ' progress-bar-danger progress-bar-striped';
           }
 
           // draw the updated progress bar
