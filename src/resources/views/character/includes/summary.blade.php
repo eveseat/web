9191ddffea49f43cfd37cd0dfca89951fded80e3
Loïc LEUILLIOT <loic.leuilliot@gmail.com>
2019-10-15 15:38:54 +0200

    feat(deps): upgrade character summary

--- src/resources/views/character/includes/summary.blade.php
+++ src/resources/views/character/includes/summary.blade.php
@@ -1,101 +1,91 @@
-<div class="panel panel-default">
-  <div class="panel-heading">
-    <h3 class="panel-title">{{ trans('web::seat.summary') }}</h3>
+<div class="card card-gray card-outline">
+  <div class="card-header">
+    <h3 class="card-title">{{ trans('web::seat.summary') }}</h3>
   </div>
-  <div class="panel-body">
+  <div class="card-body box-profile">
 
-    <div class="box-body box-profile">
+    <div class="text-center">
 
-      {!! img('character', $summary->character_id, 128, ['class' => 'profile-user-img img-responsive img-circle']) !!}
-      <h3 class="profile-username text-center">
-        {{ $summary->name }}
-      </h3>
+      {!! img('character', $summary->character_id, 128, ['class' => 'profile-user-img img-fluid img-circle']) !!}
 
-      <p class="text-muted text-center"><span class="id-to-name"
-                                              data-id="{{ $summary->corporation_id }}">{{ trans('web::seat.unknown') }}</span>
-      </p>
-
-      <table class="table table-condensed table-hover">
-        <thead></thead>
-        <tbody>
-
-        @foreach($characters as $character)
-
-          @if($character->name != $summary->name)
-
-            <tr>
-              <td>
-
-                <a href="{{ route(\Illuminate\Support\Facades\Route::currentRouteName(),
-                 array_merge(request()->route()->parameters, ['character_id' => $character->character_id])) }}">
-                  {!! img('character', $character->character_id, 64, ['class' => 'img-circle eve-icon small-icon']) !!}
-                  {{ $character->name }}
-                </a>
-
-                <p class="text-muted pull-right">
-                  <span class="id-to-name" data-id="{{ is_null($character->character) ? 0 : $character->character->corporation_id }}">{{ trans('web::seat.unknown') }}</span>
-                </p>
-              </td>
-            </tr>
-
-          @endif
-
-        @endforeach
-
-        </tbody>
-      </table>
-
-      <hr>
-
-      <dl>
-
-        <dt>{{ trans('web::seat.joined_curr_corp') }}</dt>
-        <dd>
-            @if(!is_null($summary->current_corporation))
-              {{ human_diff($summary->current_corporation->start_date) }}
-            @endif
-        </dd>
-
-        @if(auth()->user()->has('character.skills'))
-          <dt>{{ trans_choice('web::seat.skillpoint', 2) }}</dt>
-          <dd>{{ number($summary->skills->sum('skillpoints_in_skill'), 0) }}</dd>
-        @endif
-
-        @if(auth()->user()->has('character.journal') || auth()->user()->has('character.transactions'))
-        @if(! is_null($summary->balance))
-        <dt>{{ trans('web::seat.account_balance') }}</dt>
-        <dd>{{ number($summary->balance->balance) }}</dd>
-        @endif
-        @endif
-
-        @if (! is_null($summary->ship) && ! is_null($summary->ship->type))
-        <dt>{{ trans('web::seat.current_ship') }}</dt>
-        <dd>{{ $summary->ship->type->typeName }} called <i>{{ $summary->ship->ship_name }}</i></dd>
-        @endif
-
-        @if (! is_null($summary->location))
-        <dt>{{ trans('web::seat.last_location') }}</dt>
-        <dd>@include('web::partials.location', ['location' => $summary->location])</dd>
-        @endif
-
-        <dt>{{ trans('web::seat.security_status') }}</dt>
-        <dd>
-          @if($summary->security_status < 0)
-            <span class="text-danger">
-              {{ number($summary->security_status, 12) }}
-            </span>
-          @else
-            <span class="text-success">
-              {{ number($summary->security_status, 12) }}
-            </span>
-          @endif
-        </dd>
-
-      </dl>
     </div>
+    <h3 class="profile-username text-center">
+      {{ $summary->name }}
+    </h3>
 
+    <p class="text-muted text-center">
+      <span class="id-to-name" data-id="{{ $summary->corporation_id }}">{{ trans('web::seat.unknown') }}</span>
+    </p>
+
+    <ul class="list-group list-group-unbordered mb-3">
+      @foreach($characters as $character)
+
+        @if($character->name != $summary->name)
+
+          <li class="list-group-item">
+
+            <a href="{{ route(\Illuminate\Support\Facades\Route::currentRouteName(),
+             array_merge(request()->route()->parameters, ['character_id' => $character->character_id])) }}">
+              {!! img('character', $character->character_id, 64, ['class' => 'img-circle eve-icon small-icon']) !!}
+              {{ $character->name }}
+            </a>
+
+            <span class="id-to-name text-muted float-right" data-id="{{ is_null($character->character) ? 0 : $character->character->corporation_id }}">{{ trans('web::seat.unknown') }}</span>
+          </li>
+
+        @endif
+
+      @endforeach
+
+    </ul>
+
+    <dl>
+
+      <dt>{{ trans('web::seat.joined_curr_corp') }}</dt>
+      <dd>
+          @if(!is_null($summary->current_corporation))
+            {{ human_diff($summary->current_corporation->start_date) }}
+          @endif
+      </dd>
+
+      @if(auth()->user()->has('character.skills'))
+        <dt>{{ trans_choice('web::seat.skillpoint', 2) }}</dt>
+        <dd>{{ number($summary->skills->sum('skillpoints_in_skill'), 0) }}</dd>
+      @endif
+
+      @if(auth()->user()->has('character.journal') || auth()->user()->has('character.transactions'))
+      @if(! is_null($summary->balance))
+      <dt>{{ trans('web::seat.account_balance') }}</dt>
+      <dd>{{ number($summary->balance->balance) }}</dd>
+      @endif
+      @endif
+
+      @if (! is_null($summary->ship) && ! is_null($summary->ship->type))
+      <dt>{{ trans('web::seat.current_ship') }}</dt>
+      <dd>{{ $summary->ship->type->typeName }} called <i>{{ $summary->ship->ship_name }}</i></dd>
+      @endif
+
+      @if (! is_null($summary->location))
+      <dt>{{ trans('web::seat.last_location') }}</dt>
+      <dd>@include('web::partials.location', ['location' => $summary->location])</dd>
+      @endif
+
+      <dt>{{ trans('web::seat.security_status') }}</dt>
+      <dd>
+        @if($summary->security_status < 0)
+          <span class="text-danger">
+            {{ number($summary->security_status, 12) }}
+          </span>
+        @else
+          <span class="text-success">
+            {{ number($summary->security_status, 12) }}
+          </span>
+        @endif
+      </dd>
+
+    </dl>
   </div>
-  <div class="panel-footer">
+  <div class="card-footer">
     <span class="text-center center-block">
       <a href="http://eveskillboard.com/pilot/{{ $summary->name }}"
          target="_blank">
