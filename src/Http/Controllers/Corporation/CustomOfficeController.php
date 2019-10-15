f547b8cc6fedfa94390b44aa95efcd7754f8cfb6
Loïc LEUILLIOT <loic.leuilliot@gmail.com>
2019-10-15 21:50:51 +0200

    feat(deps): upgrade corporations customs-offices

---
+++ src/Http/Controllers/Corporation/CustomOfficeController.php
@@ -0,0 +1,45 @@
+<?php
+/*
+ * This file is part of SeAT
+ *
+ * Copyright (C) 2015, 2016, 2017, 2018, 2019  Leon Jacobs
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along
+ * with this program; if not, write to the Free Software Foundation, Inc.,
+ * 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+ */
+
+namespace Seat\Web\Http\Controllers\Corporation;
+
+use Seat\Web\Http\Controllers\Controller;
+use Seat\Web\Http\DataTables\Corporation\Industrial\CustomOfficeDataTable;
+use Seat\Web\Http\DataTables\Scopes\CorporationScope;
+
+/**
+ * Class CustomOfficeController
+ *
+ * @package Seat\Web\Http\Controllers\Corporation
+ */
+class CustomOfficeController extends Controller
+{
+    /**
+     * @param int $corporation_id
+     * @param \Seat\Web\Http\DataTables\Corporation\Industrial\CustomOfficeDataTable $data_table
+     * @return mixed
+     */
+    public function index(int $corporation_id, CustomOfficeDataTable $data_table)
+    {
+        return $data_table->addScope(new CorporationScope([$corporation_id]))
+            ->render('web::corporation.pocos');
+    }
+}
