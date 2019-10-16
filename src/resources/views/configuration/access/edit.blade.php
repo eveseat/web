0e56bfd68b8196de74d5e9797aa80209327d924f
Loïc LEUILLIOT <loic.leuilliot@gmail.com>
2019-10-16 19:19:06 +0200

    feat(deps): upgrade access card

--- src/resources/views/configuration/access/edit.blade.php
+++ src/resources/views/configuration/access/edit.blade.php
@@ -6,11 +6,11 @@
 
 @section('left')
 
-  <div class="panel panel-default">
-    <div class="panel-heading">
-      <h3 class="panel-title">{{ trans_choice('web::seat.permission', 2) }}</h3>
+  <div class="card">
+    <div class="card-header">
+      <h3 class="card-title">{{ trans_choice('web::seat.permission', 2) }}</h3>
     </div>
-    <div class="panel-body">
+    <div class="card-body">
 
       <form role="form" action="{{ route('configuration.access.roles.edit.permissions') }}" method="post">
         {{ csrf_field() }}
@@ -27,7 +27,7 @@
                 @foreach($permission as $category_permission)
 
                   <option value="{{ $type }}.{{ $category_permission }}">
-                    {{ studly_case($type) }}{{ studly_case($category_permission) }}
+                    {{ Str::studly($type) }}{{ Str::studly($category_permission) }}
                   </option>
 
                 @endforeach
@@ -35,7 +35,7 @@
               @else
 
                 <option value="{{ $permission }}">
-                  {{ studly_case($permission) }}
+                  {{ Str::studly($permission) }}
                 </option>
 
               @endif
@@ -52,45 +52,45 @@
           </label>
         </div>
 
-        <button type="submit" class="btn btn-success btn-block">
+        <button type="submit" class="btn btn-success btn-block mb-3">
           {{ trans('web::seat.grant_permissions') }}
         </button>
 
       </form>
 
-      <hr>
-
-      <table class="table table-hover table-condensed">
+      <table class="table table-sm table-hover table-condensed table-striped">
+        <thead>
+          <tr>
+            <th colspan="3" class="text-center">{{ trans('web::seat.current_permissions') }}</th>
+          </tr>
+        </thead>
         <tbody>
 
-        <tr>
-          <th colspan="3" class="text-center">{{ trans('web::seat.current_permissions') }}</th>
-        </tr>
+          @foreach($role->permissions as $permission)
 
-        @foreach($role->permissions as $permission)
+            <tr>
+              <td>{{ Str::studly($permission->title) }}</td>
+              <td>
+                @if($permission->pivot->not == 1)
+                  {{ trans('web::seat.inverse') }}
+                @endif
+              </td>
+              <td>
+                <a href="{{ route('configuration.access.roles.edit.remove.permission', ['role_id' => $role->id, 'permission_id' => $permission->id]) }}"
+                   type="button" class="btn btn-danger btn-sm float-right">
+                  <i class="fas fa-trash-alt"></i>
+                  {{ trans('web::seat.remove') }}
+                </a>
+              </td>
+            </tr>
 
-          <tr>
-            <td>{{ studly_case($permission->title) }}</td>
-            <td>
-              @if($permission->pivot->not == 1)
-                {{ trans('web::seat.inverse') }}
-              @endif
-            </td>
-            <td>
-              <a href="{{ route('configuration.access.roles.edit.remove.permission', ['role_id' => $role->id, 'permission_id' => $permission->id]) }}"
-                 type="button" class="btn btn-danger btn-xs float-right">
-                {{ trans('web::seat.remove') }}
-              </a>
-            </td>
-          </tr>
-
-        @endforeach
+          @endforeach
 
         </tbody>
       </table>
 
     </div>
-    <div class="panel-footer">
+    <div class="card-footer">
       <b>{{ count($role->permissions) }}</b> {{ trans_choice('web::seat.permission', count($role->permissions)) }}
 
       {{-- determine if this role has the superuser role --}}
@@ -115,11 +115,11 @@
 
 @section('center')
 
-  <div class="panel panel-default">
-    <div class="panel-heading">
-      <h3 class="panel-title">{{ trans_choice('web::seat.affiliation', 2) }}</h3>
+  <div class="card">
+    <div class="card-header">
+      <h3 class="card-title">{{ trans_choice('web::seat.affiliation', 2) }}</h3>
     </div>
-    <div class="panel-body">
+    <div class="card-body">
 
       <form role="form" action="{{ route('configuration.access.roles.edit.affiliations') }}" method="post">
         {{ csrf_field() }}
@@ -151,62 +151,62 @@
 
         </div>
 
-        <button type="submit" class="btn btn-success btn-block">
+        <button type="submit" class="btn btn-success btn-block mb-3">
           {{ trans('web::seat.add_affiliations') }}
         </button>
 
       </form>
 
-      <hr>
-
-      <table class="table table-hover table-condensed">
+      <table class="table table-sm table-hover table-condensed table-striped">
+        <thead>
+          <tr>
+            <th colspan="4" class="text-center">{{ trans('web::seat.current_affiliations') }}</th>
+          </tr>
+        </thead>
         <tbody>
 
-        <tr>
-          <th colspan="4" class="text-center">{{ trans('web::seat.current_affiliations') }}</th>
-        </tr>
+          @foreach($role->affiliations as $affiliation)
 
-        @foreach($role->affiliations as $affiliation)
+            <tr>
+              <td>
+                @if($affiliation->affiliation === 0)
 
-          <tr>
-            <td>
-              @if($affiliation->affiliation === 0)
+                  {{ trans('web::seat.all') }}
+                  @if($affiliation->type == 'corp')
+                    {{ trans_choice('web::seat.corporation', 2) }}
+                  @else
+                    {{ trans_choice('web::seat.character', 2) }}
+                  @endif
 
-                {{ trans('web::seat.all') }}
-                @if($affiliation->type == 'corp')
-                  {{ trans_choice('web::seat.corporation', 2) }}
                 @else
-                  {{ trans_choice('web::seat.character', 2) }}
+
+                  {!! img('auto', $affiliation->affiliation, 64, ['class' => 'img-circle eve-icon small-icon']) !!}
+                  <span class="id-to-name" data-id="{{$affiliation->affiliation}}">{{ trans('web::seat.unknown') }}</span>
+
                 @endif
+              </td>
+              <td>{{ ucfirst($affiliation->type) }}</td>
+              <td>
+                @if($affiliation->pivot->not == 1)
+                  {{ trans('web::seat.inverse') }}
+                @endif
+              </td>
+              <td>
+                <a href="{{ route('configuration.access.roles.edit.remove.affiliation', ['role_id' => $role->id, 'user_id' => $affiliation->id]) }}"
+                   type="button" class="btn btn-danger btn-sm float-right">
+                  <i class="fas fa-trash-alt"></i>
+                  {{ trans('web::seat.remove') }}
+                </a>
+              </td>
+            </tr>
 
-              @else
-
-                {!! img('auto', $affiliation->affiliation, 64, ['class' => 'img-circle eve-icon small-icon']) !!}
-                <span class="id-to-name" data-id="{{$affiliation->affiliation}}">{{ trans('web::seat.unknown') }}</span>
-
-              @endif
-            </td>
-            <td>{{ ucfirst($affiliation->type) }}</td>
-            <td>
-              @if($affiliation->pivot->not == 1)
-                {{ trans('web::seat.inverse') }}
-              @endif
-            </td>
-            <td>
-              <a href="{{ route('configuration.access.roles.edit.remove.affiliation', ['role_id' => $role->id, 'user_id' => $affiliation->id]) }}"
-                 type="button" class="btn btn-danger btn-xs float-right">
-                {{ trans('web::seat.remove') }}
-              </a>
-            </td>
-          </tr>
-
-        @endforeach
+          @endforeach
 
         </tbody>
       </table>
 
     </div>
-    <div class="panel-footer">
+    <div class="card-footer">
       <b>{{ count($role->affiliations) }}</b> {{ trans_choice('web::seat.affiliation', count($role->affiliations)) }}
     </div>
   </div>
@@ -215,11 +215,11 @@
 
 @section('right')
 
-  <div class="panel panel-default">
-    <div class="panel-heading">
-      <h3 class="panel-title">{{ trans_choice('web::seat.group', 2) }}</h3>
+  <div class="card">
+    <div class="card-header">
+      <h3 class="card-title">{{ trans_choice('web::seat.group', 2) }}</h3>
     </div>
-    <div class="panel-body">
+    <div class="card-body">
 
       <form role="form" action="{{ route('configuration.access.roles.edit.groups') }}" method="post">
         {{ csrf_field() }}
@@ -231,40 +231,40 @@
           </select>
         </div>
 
-        <button type="submit" class="btn btn-success btn-block">{{ trans_choice('web::seat.add_group', 2) }}</button>
+        <button type="submit" class="btn btn-success btn-block mb-3">{{ trans_choice('web::seat.add_group', 2) }}</button>
 
       </form>
 
-      <hr>
-
-      <table class="table table-hover table-condensed">
+      <table class="table table-sm table-hover table-condensed table-striped">
+        <thead>
+          <tr>
+            <th colspan="2" class="text-center">{{ trans_choice('web::seat.current_groups',2) }}</th>
+          </tr>
+        </thead>
         <tbody>
 
-        <tr>
-          <th colspan="2" class="text-center">{{ trans_choice('web::seat.current_groups',2) }}</th>
-        </tr>
+          @foreach($role->groups as $group)
 
-        @foreach($role->groups as $group)
+            <tr>
+              <td>
+                {{ $group->users->map(function($user) { return $user->name; })->implode(', ') }}
+              </td>
+              <td>
+                <a href="{{ route('configuration.access.roles.edit.remove.group', ['role_id' => $role->id, 'user_id' => $group->id]) }}"
+                   type="button" class="btn btn-danger btn-sm float-right">
+                  <i class="fas fa-trash-alt"></i>
+                  {{ trans('web::seat.remove') }}
+                </a>
+              </td>
+            </tr>
 
-          <tr>
-            <td>
-              {{ $group->users->map(function($user) { return $user->name; })->implode(', ') }}
-            </td>
-            <td>
-              <a href="{{ route('configuration.access.roles.edit.remove.group', ['role_id' => $role->id, 'user_id' => $group->id]) }}"
-                 type="button" class="btn btn-danger btn-xs float-right">
-                {{ trans('web::seat.remove') }}
-              </a>
-            </td>
-          </tr>
-
-        @endforeach
+          @endforeach
 
         </tbody>
       </table>
 
     </div>
-    <div class="panel-footer">
+    <div class="card-footer">
         <b>{{ count($role->groups) }}</b>
         {{ trans_choice('web::seat.group', count($role->groups)) }}
     </div>
