0008033263ebe72747c1689dc7e051ec7ebdeeaf
Loïc LEUILLIOT <loic.leuilliot@gmail.com>
2019-10-16 10:56:55 +0200

    feat(deps): upgrade corporations ledger sub-menu

--- src/resources/views/corporation/ledger/includes/menu.blade.php
+++ src/resources/views/corporation/ledger/includes/menu.blade.php
@@ -1,18 +1,18 @@
-<div style="padding-bottom: 15px;">
+<div class="pb-3">
 
   <ul class="nav nav-pills">
-    <li role="presentation" class="@if ($sub_viewname == 'summary') active @endif">
-      <a href="{{ route('corporation.view.ledger.summary', $sheet->corporation_id) }}">
+    <li role="presentation" class="nav-item">
+      <a href="{{ route('corporation.view.ledger.summary', $sheet->corporation_id) }}" class="nav-link @if ($sub_viewname == 'summary') active @endif">
         {{ trans_choice('web::seat.wallet_summary', 1) }}
       </a>
     </li>
-    <li role="presentation" class="@if ($sub_viewname == 'bountyprizesbymonth') active @endif">
-      <a href="{{ route('corporation.view.ledger.bountyprizesbymonth', $sheet->corporation_id) }}">
+    <li role="presentation" class="nav-item">
+      <a href="{{ route('corporation.view.ledger.bountyprizesbymonth', $sheet->corporation_id) }}" class="nav-link @if ($sub_viewname == 'bountyprizesbymonth') active @endif">
         {{ trans_choice('web::seat.bountyprizesbymonth', 2) }}
       </a>
     </li>
-    <li role="presentation" class="@if ($sub_viewname == 'planetaryinteraction') active @endif">
-      <a href="{{ route('corporation.view.ledger.planetaryinteraction', $sheet->corporation_id) }}">
+    <li role="presentation" class="nav-item">
+      <a href="{{ route('corporation.view.ledger.planetaryinteraction', $sheet->corporation_id) }}" class="nav-link @if ($sub_viewname == 'planetaryinteraction') active @endif">
         {{ trans_choice('web::seat.pi', 2) }}
       </a>
     </li>
