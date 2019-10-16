fe57af783b8d45a47b3d2391302eecc88937e6a5
Loïc LEUILLIOT <loic.leuilliot@gmail.com>
2019-10-16 16:12:04 +0200

    feat(deps): upgrade corporations bookmarks

--- src/resources/views/corporation/bookmarks.blade.php
+++ src/resources/views/corporation/bookmarks.blade.php
@@ -4,11 +4,11 @@
 
 @section('corporation_content')
 
-  <div class="panel panel-default">
-    <div class="panel-heading">
-      <h3 class="panel-title">{{ trans_choice('web::seat.bookmark', 2) }}</h3>
+  <div class="card">
+    <div class="card-header">
+      <h3 class="card-title">{{ trans_choice('web::seat.bookmark', 2) }}</h3>
     </div>
-    <div class="panel-body">
+    <div class="card-body">
 
       {{ $dataTable->table() }}
 
