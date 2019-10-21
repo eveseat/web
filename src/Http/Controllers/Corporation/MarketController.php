136e8be87128dfa33ffb6caa3ce5931f63fc498f
Loïc LEUILLIOT <loic.leuilliot@gmail.com>
2019-10-21 15:06:49 +0200

    feat(market): add filters options on characters & corporation market
    Closes eveseat/seat#266

--- src/Http/Controllers/Corporation/MarketController.php
+++ src/Http/Controllers/Corporation/MarketController.php
@@ -25,6 +25,8 @@ namespace Seat\Web\Http\Controllers\Corporation;
 use Seat\Web\Http\Controllers\Controller;
 use Seat\Web\Http\DataTables\Corporation\Financial\MarketDataTable;
 use Seat\Web\Http\DataTables\Scopes\CorporationScope;
+use Seat\Web\Http\DataTables\Scopes\Filters\MarketOrderTypeScope;
+use Seat\Web\Http\DataTables\Scopes\Filters\MarketStatusScope;
 
 /**
  * Class MarketController.
@@ -41,6 +43,8 @@ class MarketController extends Controller
     {
 
         return $dataTable->addScope(new CorporationScope([$corporation_id]))
+            ->addScope(new MarketStatusScope(request()->input('filters.status')))
+            ->addScope(new MarketOrderTypeScope(request()->input('filters.type')))
             ->render('web::corporation.market');
     }
 }
---
+++ src/Http/DataTables/Scopes/Filters/MarketOrderTypeScope.php
@@ -0,0 +1,68 @@
+<?php
+
+/*
+ * This file is part of SeAT
+ *
+ * Copyright (C) 2015, 2016, 2017, 2018, 2019  Leon Jacobs
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along
+ * with this program; if not, write to the Free Software Foundation, Inc.,
+ * 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+ */
+
+namespace Seat\Web\Http\DataTables\Scopes\Filters;
+
+use Yajra\DataTables\Contracts\DataTableScope;
+
+/**
+ * Class MarketOrderTypeScope.
+ *
+ * This is a filter scope for market tables.
+ * It will restrict returned data based on order type.
+ *
+ * @package Seat\Web\Http\DataTables\Scopes\Filters
+ */
+class MarketOrderTypeScope implements DataTableScope
+{
+    /**
+     * @var array
+     */
+    private $order_type = [];
+
+    /**
+     * MarketOrderTypeScope constructor.
+     *
+     * @param array $corporation_ids
+     */
+    public function __construct(?array $order_type)
+    {
+        $this->order_type = $order_type ?: [];
+    }
+
+    /**
+     * Apply a query scope.
+     *
+     * @param \Illuminate\Database\Query\Builder|\Illuminate\Database\Eloquent\Builder $query
+     * @return mixed
+     */
+    public function apply($query)
+    {
+        if (in_array(0, $this->order_type))
+            return $query->where(function ($sub_query) {
+                $sub_query->whereIn('is_buy_order', $this->order_type)
+                    ->orWhereNull('is_buy_order');
+            });
+
+        return $query->whereIn('is_buy_order', $this->order_type);
+    }
+}
