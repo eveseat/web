d129e0a56da61fbce9978d80e6deebc667e016ee
Loïc Leuilliot <loic.leuilliot@gmail.com>
2019-10-22 12:22:28 +0200

    feat(market): add filters options on characters & corporation market (#322)
    Closes eveseat/seat#266

--- src/resources/views/character/market.blade.php
+++ src/resources/views/character/market.blade.php
@@ -29,6 +29,34 @@
           @endforeach
         </select>
       </div>
+      <div class="mb-3">
+        <div class="btn-group d-flex">
+          <button data-filter="pending" id="dt-filters-pending" class="btn btn-primary active dt-filters-status">
+            <i class="fas fa-play"></i>
+            Pending
+          </button>
+          <button data-filter="expired" id="dt-filters-expired" class="btn btn-danger dt-filters-status">
+            <i class="fas fa-history"></i>
+            Expired
+          </button>
+          <button data-filter="completed" id="dt-filters-completed" class="btn btn-secondary dt-filters-status">
+            <i class="fas fa-check"></i>
+            Completed
+          </button>
+        </div>
+      </div>
+      <div class="mb-3">
+        <div class="btn-group d-flex">
+          <button data-filter="0" id="dt-filters-sell" class="btn btn-light active dt-filters-type">
+            <i class="fas fa-caret-up"></i>
+            Sell Orders
+          </button>
+          <button data-filter="1" id="dt-filters-buy" class="btn btn-light active dt-filters-type">
+            <i class="fas fa-caret-down"></i>
+            Buy Orders
+          </button>
+        </div>
+      </div>
 
       {!! $dataTable->table() !!}
     </div>
@@ -44,6 +72,12 @@
         $('#dt-character-selector')
             .select2()
             .on('change', function () {
+                window.LaravelDataTables['dataTableBuilder'].ajax.reload();
+            });
+
+        $('#dt-filters-pending, #dt-filters-expired, #dt-filters-completed, #dt-filters-sell, #dt-filters-buy')
+            .on('click', function () {
+                $(this).hasClass('active') ? $(this).removeClass('active') : $(this).addClass('active');
                 window.LaravelDataTables['dataTableBuilder'].ajax.reload();
             });
     });
