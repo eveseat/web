49566e91cc1eedc5c061217f7d49faec3be72c6f
Loïc LEUILLIOT <loic.leuilliot@gmail.com>
2019-10-23 18:47:03 +0200

    fix(fittings): use button instead of a tag

--- src/resources/views/common/fittings/buttons/export.blade.php
+++ src/resources/views/common/fittings/buttons/export.blade.php
@@ -1,3 +1,3 @@
-<a href="#" title="Copy to clipboard" data-toggle="tooltip" data-export="{{ $data_export }}" class="copy-fitting">
+<button title="Copy to clipboard" data-toggle="tooltip" data-export="{{ $data_export }}" class="btn btn-sm btn-link copy-fitting">
   <i class="fa fa-copy"></i>
-</a>
+</button>
