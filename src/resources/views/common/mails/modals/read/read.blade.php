9d550379ac485baa4baffcc2557562244504e7a5
Loïc LEUILLIOT <loic.leuilliot@gmail.com>
2019-10-28 19:14:02 +0100

    feat(search): update search views to BS4

--- src/resources/views/common/mails/modals/read/read.blade.php
+++ src/resources/views/common/mails/modals/read/read.blade.php
@@ -1,5 +1,5 @@
 <div class="modal fade" id="mail-content" tabindex="-1" role="dialog" aria-labelledby="mail-content-label">
-  <div class="modal-dialog" role="document">
+  <div class="modal-dialog modal-dialog-scrollable" role="document">
     <div class="modal-content">
       <div class="modal-header bg-blue">
         <h4 class="modal-title" id="mail-content-label">{{ trans('web::seat.mail') }}</h4>
