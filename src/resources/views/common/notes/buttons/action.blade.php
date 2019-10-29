d74b73256cd5ad7417889b6075b77bf205eaf145
Loïc LEUILLIOT <loic.leuilliot@gmail.com>
2019-10-29 14:21:48 +0100

    refactor(notes): fix character note layout and improve blades structure

---
+++ src/resources/views/common/notes/buttons/action.blade.php
@@ -0,0 +1,15 @@
+<form method="post" action="{{ route('tools.notes.destroy', ['id' => $row->id]) }}">
+  {!! csrf_field() !!}
+  {!! method_field('DELETE') !!}
+  <div class="btn-group btn-group-sm float-right">
+    <button type="button" data-toggle="modal" data-target="#note-edit-modal" class="btn btn-sm btn-warning"
+            data-url="{{ route('tools.notes.update', ['id' => $row->id]) }}">
+      <i class="fas fa-pencil-alt"></i>
+      {{ trans('web::seat.edit') }}
+    </button>
+    <button type="submit" class="btn btn-sm btn-danger">
+      <i class="fas fa-trash-alt"></i>
+      {{ trans('web::seat.delete') }}
+    </button>
+  </div>
+</form>
