7a2d9fd37530ebab8a67a0d07a00a8877ef6c0ff
Loïc LEUILLIOT <loic.leuilliot@gmail.com>
2019-10-29 16:12:01 +0100

    fix(layouts): apply BS4 to intel menu

--- src/resources/views/character/intel/includes/menu.blade.php
+++ src/resources/views/character/intel/includes/menu.blade.php
@@ -1,18 +1,21 @@
-<div style="padding-bottom: 15px;">
+<div class="pb-3">
 
   <ul class="nav nav-pills">
-    <li role="presentation" class="@if ($sub_viewname == 'summary') active @endif">
-      <a href="{{ route('character.view.intel.summary', $summary->character_id) }}">
+    <li role="presentation" class="nav-item">
+      <a href="{{ route('character.view.intel.summary', $summary->character_id) }}"
+         class="{{ $sub_viewname == 'summary' ? 'nav-link active' : 'nav-link' }}">
         Summary
       </a>
     </li>
-    <li role="presentation" class="@if ($sub_viewname == 'standings') active @endif">
-      <a href="{{ route('character.view.intel.standingscomparison', $summary->character_id) }}">
+    <li role="presentation" class="nav-item">
+      <a href="{{ route('character.view.intel.standingscomparison', $summary->character_id) }}"
+         class="{{ $sub_viewname == 'standings' ? 'nav-link active' : 'nav-link' }}">
         Standings Compare
       </a>
     </li>
-    <li role="presentation" class="@if ($sub_viewname == 'note') active @endif">
-      <a href="{{ route('character.view.intel.notes', $summary->character_id) }}">
+    <li role="presentation" class="nav-item">
+      <a href="{{ route('character.view.intel.notes', $summary->character_id) }}"
+         class="{{ $sub_viewname == 'note' ? 'nav-link active' : 'nav-link' }}">
         Notes
       </a>
     </li>
