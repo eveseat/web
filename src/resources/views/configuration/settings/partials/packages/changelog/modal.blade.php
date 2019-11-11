a00b34daa5df0a9065b8eb08a5ac9df57ce5574d
Loïc LEUILLIOT <loic.leuilliot@gmail.com>
2019-11-11 15:26:52 +0100

    fix(ui): apply small change on UI layouts

--- src/resources/views/configuration/settings/partials/packages/changelog/modal.blade.php
+++ src/resources/views/configuration/settings/partials/packages/changelog/modal.blade.php
@@ -1,11 +1,11 @@
 <div class="modal fade" tabindex="-1" role="dialog" id="changelogModal">
-  <div class="modal-dialog" role="document">
+  <div class="modal-dialog modal-xl" role="document">
     <div class="modal-content">
       <div class="modal-header">
+        <h3 class="modal-title">Changelog for <span></span></h3>
         <button type="button" class="close" data-dismiss="modal" aria-label="Close">
           <span aria-hidden="true">&times;</span>
         </button>
-        <h3 class="modal-title">Changelog for <span></span></h3>
       </div>
       <div class="modal-body"></div>
     </div>
