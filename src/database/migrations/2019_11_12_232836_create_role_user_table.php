50db74f2eb2f97bf723783c268741f1dc8c559ef
Loïc LEUILLIOT <loic.leuilliot@gmail.com>
2019-11-16 14:34:01 +0100

    style: apply syleci change requests

--- src/database/migrations/2019_11_12_232836_create_role_user_table.php
+++ src/database/migrations/2019_11_12_232836_create_role_user_table.php
@@ -25,7 +25,7 @@ use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
 /**
- * Class CreateRoleUserTable
+ * Class CreateRoleUserTable.
  */
 class CreateRoleUserTable extends Migration
 {
