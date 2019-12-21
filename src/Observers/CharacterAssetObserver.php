b1e486d92037fcf9489d98994885284ee5198f4d
Loïc LEUILLIOT <loic.leuilliot@gmail.com>
2019-12-21 18:32:56 +0100

    feat(squads): allow user roles to be auto updated
    register new observers to assets, skills and affiliation
    register new observers to squad and its pivots
    
    when a user is added to a squad, he will automatically gain attached roles
    when a user is removed from a squad, he will automatically loose related roles
    when a role is added to a squad, members will automatically gain it
    when a role is removed from a squad, members will automatically loose it

---
+++ src/Observers/CharacterAssetObserver.php
@@ -0,0 +1,70 @@
+<?php
+
+/*
+ * This file is part of SeAT
+ *
+ * Copyright (C) 2015, 2016, 2017, 2018, 2019  Leon Jacobs
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along
+ * with this program; if not, write to the Free Software Foundation, Inc.,
+ * 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+ */
+
+namespace Seat\Web\Observers;
+
+use Illuminate\Database\Eloquent\Model;
+use Seat\Eveapi\Models\Assets\CharacterAsset;
+use Seat\Web\Models\User;
+
+/**
+ * Class CharacterAssetObserver.
+ *
+ * @package Seat\Web\Observers
+ */
+class CharacterAssetObserver extends AbstractSquadObserver
+{
+    /**
+     * @param \Seat\Eveapi\Models\Assets\CharacterAsset $asset
+     */
+    public function created(CharacterAsset $asset)
+    {
+        $this->updateUserSquads($asset);
+    }
+
+    /**
+     * @param \Seat\Eveapi\Models\Assets\CharacterAsset $asset
+     */
+    public function updated(CharacterAsset $asset)
+    {
+        $this->updateUserSquads($asset);
+    }
+
+    /**
+     * @param \Seat\Eveapi\Models\Assets\CharacterAsset $asset
+     */
+    public function deleted(CharacterAsset $asset)
+    {
+        $this->updateUserSquads($asset);
+    }
+
+    /**
+     * @inheritDoc
+     */
+    protected function findRelatedUser(Model $fired_model): ?User
+    {
+        // retrieve user related to the character affiliation
+        return User::with('squads')->whereHas('characters', function ($query) use ($fired_model) {
+            $query->where('character_infos.character_id', $fired_model->character_id);
+        })->first();
+    }
+}
