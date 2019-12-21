9cf18f56e6c64858213320f7f22d352380a8816b
Loïc LEUILLIOT <loic.leuilliot@gmail.com>
2019-12-21 18:30:19 +0100

    refactor: drop has filter check from filter trait

--- src/resources/views/components/filters/buttons/filters.blade.php
+++ src/resources/views/components/filters/buttons/filters.blade.php
@@ -1,4 +1,3 @@
-{{-- <button data-toggle="modal" data-target="#filters-modal" class="btn btn-warning" data-filters="{{ json_encode($rules ?? '{"and":[]}') }}"> --}}
-<button data-toggle="modal" data-target="#filters-modal" class="btn btn-warning" id="filters-btn" data-filters="{{ $rules ? json_encode($rules) : '{}' }}">
+<button data-toggle="modal" data-target="#filters-modal" class="btn btn-sm btn-warning" id="filters-btn" data-filters="{{ $rules ? json_encode($rules) : '{}' }}">
   <i class="fas fa-sliders-h"></i> Filters
 </button>
