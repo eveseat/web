0b41427a8891fbccc2516bbd6b5e04b4e1d69ff0
Loïc LEUILLIOT <loic.leuilliot@gmail.com>
2019-12-29 12:12:55 +0100

    refactor(squads): improve squad card layout

--- src/resources/views/squads/buttons/squads/join.blade.php
+++
@@ -1,3 +0,0 @@
-<button data-toggle="modal" data-target="#application-create" class="btn btn-success float-right">
-  <i class="fas fa-sign-in-alt"></i> {{ trans('web::squads.join') }}
-</button>
--- src/resources/views/squads/buttons/squads/leave.blade.php
+++
@@ -1,7 +0,0 @@
-<form method="post" action="{{ route('squads.members.leave', $squad->id) }}" id="form-leave">
-  {!! csrf_field() !!}
-  {!! method_field('DELETE') !!}
-</form>
-<button type="submit" class="btn btn-danger" form="form-leave">
-  <i class="fas fa-sign-out-alt"></i> {{ trans('web::squads.leave') }}
-</button>
