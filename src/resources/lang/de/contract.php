e82da8e95feaf718758a6f9c1a789b3d30c50d5d
Peter Pfeufer <info@ppfeufer.de>
2020-01-11 19:45:03 +0100

    [translation] contracts to german

---
+++ src/resources/lang/de/contract.php
@@ -0,0 +1,51 @@
+<?php
+
+/*
+ * This file is part of SeAT
+ *
+ * Copyright (C) 2015, 2016, 2017, 2018, 2019  Leon Jacobs
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along
+ * with this program; if not, write to the Free Software Foundation, Inc.,
+ * 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+ */
+
+return [
+    'created'             => 'Erstellt',
+    'type'                => 'Typ',
+    'issuer'              => 'Aussteller',
+    'assignee'            => 'Ausgetsellt an',
+    'acceptor'            => 'Angenommen von',
+    'status'              => 'Status',
+    'price'               => 'Preis',
+    'reward'              => 'Belohnung',
+
+    'unknown'             => 'Unbekannt',
+    'item_exchange'       => 'Gegenstandstausch',
+    'auction'             => 'Auktion',
+    'courier'             => 'Kurier',
+    'loan'                => 'Darlehen',
+
+    'outstanding'         => 'Ausstehend',
+    'in_progress'         => 'In Bearbeitung',
+    'finished_issuer'     => 'Aussteller beendet',
+    'finished_contractor' => 'Auftragenehmer beendet',
+    'finished'            => 'Beendet',
+    'cancelled'           => 'Abgebrochen',
+    'rejected'            => 'Abgelehnt',
+    'failed'              => 'Fehlgeschlagen',
+    'deleted'             => 'Gelöscht',
+    'reversed'            => 'Rückgängig gemacht',
+
+    'not_accepted'        => 'Nicht akzeptiert',
+];
