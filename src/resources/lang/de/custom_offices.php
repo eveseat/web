58e8d59355fb20bda6ec2e5c2539d681a55c6ecc
Peter Pfeufer <info@ppfeufer.de>
2020-01-11 19:48:16 +0100

    [translation] custom offices to german

---
+++ src/resources/lang/de/custom_offices.php
@@ -0,0 +1,31 @@
+<?php
+
+/*
+ * This file is part of SeAT
+ *
+ * Copyright (C) 2015, 2016, 2017, 2018, 2019  Leon Jacobs
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along
+ * with this program; if not, write to the Free Software Foundation, Inc.,
+ * 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+ */
+
+return [
+    'planet'            => 'Planet',
+    'reinforcement'     => 'Rf. Periode', // in lack of a good german translation, I leave this here, because the translation of reinforcement makes no sense in german for that matter.
+    'alliance'          => 'Erlaube Zugruff durch Allianz',
+    'standings'         => 'Erlaube Zugriff durch Ansehen',
+    'standing_level'    => 'Minimales Ansehen',
+    'tax_alliance_corp' => 'Steuern Allianz/Corp',
+    'tax_standings'     => 'Steuern Ansehen',
+];
