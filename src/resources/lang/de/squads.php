be96c20ed115ca3de8378499d088dc41d23619fd
Peter Pfeufer <info@ppfeufer.de>
2020-01-11 21:26:18 +0100

    [translation] squads to german

---
+++ src/resources/lang/de/squads.php
@@ -0,0 +1,44 @@
+<?php
+
+/*
+ * This file is part of SeAT
+ *
+ * Copyright (C) 2015, 2016, 2017, 2018, 2019  Leon Jacobs
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along
+ * with this program; if not, write to the Free Software Foundation, Inc.,
+ * 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+ */
+
+return [
+    'squad'        => 'Squad(s)',
+    'name'         => 'Name(n)',
+    'description'  => 'Beschreibung',
+    'type'         => 'Typ',
+    'member'       => 'Mitglied(er)',
+    'moderated'    => 'Moderiert',
+    'moderator'    => 'Moderator(en)',
+    'candidate'    => 'Kandidat(en)',
+    'member_since' => 'Mitglied seit',
+    'applied_at'   => 'Applied At',
+    'character'    => 'Charakter(e)',
+    'show'         => 'Zeige',
+    'delete'       => 'Löschen',
+    'approve'      => 'Genehmigen',
+    'reject'       => 'Ablehnen',
+    'kick'         => 'Rauswerfen',
+    'leave'        => 'Verlassen',
+    'join'         => 'Beitreten',
+    'message'      => 'Nachricht',
+    'remove'       => 'Entfernen',
+];
