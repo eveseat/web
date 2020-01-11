ced94af51c72deb1c7a380bceb1fdf8f0086fad3
Peter Pfeufer <info@ppfeufer.de>
2020-01-11 21:57:38 +0100

    [translation] wallet to german
    Here I struggled because I don't have anyone in my corp with a german client who has all the transaction types available. I translated what I could here. Maybe there is a way to get the propper translations from ESI/CCP somehow?

---
+++ src/resources/lang/de/wallet.php
@@ -0,0 +1,159 @@
+<?php
+
+/*
+ * This file is part of SeAT
+ *
+ * Copyright (C) 2015, 2016, 2017, 2018, 2019  Leon Jacobs
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along
+ * with this program; if not, write to the Free Software Foundation, Inc.,
+ * 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+ */
+
+return [
+    'date'       => 'Datum',
+    'ref_type'   => 'Typ',
+    'from_party' => 'Von',
+    'to_party'   => 'An',
+    'amount'     => 'Betrag',
+    'balance'    => 'Kontostand',
+
+    'order'      => 'Auftrag',
+    'type'       => 'Typ',
+    'location'   => 'Ort',
+    'price'      => 'Preis',
+    'quantity'   => 'Menge',
+    'total'      => 'Total',
+    'party'      => 'Gruppe',
+
+    'acceleration_gate_fee' => 'Sprungtor Gebühr',
+    'advertisement_listing_fee' => 'Anzeigegebühr',
+    'agent_donation' => 'Agent Auszahlung',
+    'agent_location_services' => 'Agent Lokalisierungsservice',
+    'agent_miscellaneous' => 'Agent Sonstiges',
+    'agent_mission_collateral_paid' => 'Agent Mission Collateral Paid', // Coul'd not find a useful translation
+    'agent_mission_collateral_refunded' => 'Agent Mission Collateral Refunded', // Coul'd not find a useful translation
+    'agent_mission_reward' => 'Agent Mission Belohnung',
+    'agent_mission_reward_corporation_tax' => 'Agent Mission Belohnung Corp Steuern',
+    'agent_mission_time_bonus_reward' => 'Agent Mission Zeit Bonus Belohnung',
+    'agent_mission_time_bonus_reward_corporation_tax' => 'Agent Mission Zeit Bonus Belohnung Corp Steuern',
+    'agent_security_services' => 'Agent Security Services', // Coul'd not find a useful translation
+    'agent_services_rendered' => 'Agent Services Rendered', // Coul'd not find a useful translation
+    'agents_preward' => 'Agent Belohnung',
+    'alliance_maintainance_fee' => 'Alliance Verwaltungsgebühr',
+    'alliance_registration_fee' => 'Alliance Registrierungsgebühr',
+    'asset_safety_recovery_tax' => 'Asset Safety Recovery Tax', // Coul'd not find a useful translation
+    'bounty' => 'Kopfgeld',
+    'bounty_prize' => 'Kopfgeld Auszahlung',
+    'bounty_prize_corporation_tax' => 'Kopfgeld Auszahlung Corp Steuern',
+    'bounty_prizes' => 'Kopfgeld Auszahlungen',
+    'bounty_reimbursement' => 'Bounty Reimbursement', // Coul'd not find a useful translation
+    'bounty_surcharge' => 'Bounty Surcharge', // Coul'd not find a useful translation
+    'brokers_fee' => 'Makler gebühren',
+    'clone_activation' => 'Sprungklon Aktivierungsgebühr',
+    'clone_transfer' => 'Klonüberführung',
+    'contraband_fine' => 'Schmuggelware Strafzahlung',
+    'contract_auction_bid' => 'Auktionsgebot',
+    'contract_auction_bid_corp' => 'Auktionsgebot Corp',
+    'contract_auction_bid_refund' => 'Auktionsgebot Rückzahlung',
+    'contract_auction_sold' => 'Contract Auction Sold', // Coul'd not find a useful translation
+    'contract_brokers_fee' => 'Vertrag Makler Gebühren',
+    'contract_brokers_fee_corp' => 'Contract Brokers Fee Corp', // Coul'd not find a useful translation
+    'contract_collateral' => 'Contract Collateral', // Coul'd not find a useful translation
+    'contract_collateral_deposited_corp' => 'Contract Collateral Deposited Corp', // Coul'd not find a useful translation
+    'contract_collateral_payout' => 'Contract Collateral Payout', // Coul'd not find a useful translation
+    'contract_collateral_refund' => 'Contract Collateral Refund', // Coul'd not find a useful translation
+    'contract_deposit' => 'Contract Deposit', // Coul'd not find a useful translation
+    'contract_deposit_corp' => 'Contract Deposit Corp', // Coul'd not find a useful translation
+    'contract_deposit_refund' => 'Contract Deposit Refund', // Coul'd not find a useful translation
+    'contract_deposit_sales_tax' => 'Contract Deposit Sales Tax', // Coul'd not find a useful translation
+    'contract_price' => 'Vertragspreis',
+    'contract_price_payment_corp' => 'Contract Price Payment Corp', // Coul'd not find a useful translation
+    'contract_reversal' => 'Contract Reversal', // Coul'd not find a useful translation
+    'contract_reward' => 'Contract Reward', // Coul'd not find a useful translation
+    'contract_reward_deposited' => 'Contract Reward Deposited', // Coul'd not find a useful translation
+    'contract_reward_deposited_corp' => 'Contract Reward Deposited Corp', // Coul'd not find a useful translation
+    'contract_reward_refund' => 'Contract Reward Refund', // Coul'd not find a useful translation
+    'contract_sales_tax' => 'Contract Sales Tax', // Coul'd not find a useful translation
+    'copying' => 'Copying', // Coul'd not find a useful translation
+    'corporate_reward_payout' => 'Corporate Reward Payout', // Coul'd not find a useful translation
+    'corporate_reward_tax' => 'Corporate Reward Tax', // Coul'd not find a useful translation
+    'corporation_account_withdrawal' => 'Corporation Kontoabhebung',
+    'corporation_bulk_payment' => 'Corporation Bulk Payment', // Coul'd not find a useful translation
+    'corporation_dividend_payment' => 'Corporation Dividend Payment', // Coul'd not find a useful translation
+    'corporation_liquidation' => 'Corporation Liquidation', // Coul'd not find a useful translation
+    'corporation_logo_change_cost' => 'Corporation Logo Change Cost', // Coul'd not find a useful translation
+    'corporation_payment' => 'Corporation Payment', // Coul'd not find a useful translation
+    'corporation_registration_fee' => 'Corporation Registrierungsgebühr',
+    'courier_mission_escrow' => 'Courier Mission Escrow', // Coul'd not find a useful translation
+    'cspa' => 'CSPA',
+    'cspaofflinerefund' => 'CSPA Offline Refund', // Coul'd not find a useful translation
+    'datacore_fee' => 'Datacore Fee', // Coul'd not find a useful translation
+    'dna_modification_fee' => 'Dna Modification Fee', // Coul'd not find a useful translation
+    'docking_fee' => 'Andockgebühren',
+    'duel_wager_escrow' => 'Duel Wager Escrow', // Coul'd not find a useful translation
+    'duel_wager_payment' => 'Duel Wager Payment', // Coul'd not find a useful translation
+    'duel_wager_refund' => 'Duel Wager Refund', // Coul'd not find a useful translation
+    'factory_slot_rental_fee' => 'Factory Slot Rental Fee', // Coul'd not find a useful translation
+    'gm_cash_transfer' => 'Gm Cash Transfer', // Coul'd not find a useful translation
+    'industry_job_tax' => 'Industrie Job Steuern',
+    'infrastructure_hub_maintenance' => 'Infrastructure Hub Maintenance', // Coul'd not find a useful translation
+    'inheritance' => 'Inheritance', // Coul'd not find a useful translation
+    'insurance' => 'Versicherung',
+    'item_trader_payment' => 'Item Trader Payment', // Coul'd not find a useful translation
+    'jump_clone_activation_fee' => 'Sprungklon Aktivierungskosten',
+    'jump_clone_installation_fee' => 'Sprungklon Installationskosten',
+    'kill_right_fee' => 'Kill Right Fee', // Coul'd not find a useful translation
+    'lp_store' => 'Lp Store', // Coul'd not find a useful translation
+    'manufacturing' => 'Production',
+    'market_escrow' => 'Market Escrow', // Coul'd not find a useful translation
+    'market_fine_paid' => 'Market Fine Paid', // Coul'd not find a useful translation
+    'market_transaction' => 'Market Transaction', // Coul'd not find a useful translation
+    'medal_creation' => 'Medal Creation', // Coul'd not find a useful translation
+    'medal_issued' => 'Medal Issued', // Coul'd not find a useful translation
+    'mission_completion' => 'Mission Completion', // Coul'd not find a useful translation
+    'mission_cost' => 'Mission Cost', // Coul'd not find a useful translation
+    'mission_expiration' => 'Mission Expiration', // Coul'd not find a useful translation
+    'mission_reward' => 'Mission Reward', // Coul'd not find a useful translation
+    'office_rental_fee' => 'Office Rental Fee', // Coul'd not find a useful translation
+    'operation_bonus' => 'Operation Bonus', // Coul'd not find a useful translation
+    'opportunity_reward' => 'Opportunity Reward', // Coul'd not find a useful translation
+    'planetary_construction' => 'Planetary Construction', // Coul'd not find a useful translation
+    'planetary_export_tax' => 'Planetary Export Tax', // Coul'd not find a useful translation
+    'planetary_import_tax' => 'Planetary Import Tax', // Coul'd not find a useful translation
+    'player_donation' => 'Player Donation', // Coul'd not find a useful translation
+    'player_trading' => 'Player Trading', // Coul'd not find a useful translation
+    'project_discovery_reward' => 'Project Discovery Belohnung',
+    'project_discovery_tax' => 'Project Discovery Steuern',
+    'reaction' => 'Reaktion',
+    'release_of_impounded_property' => 'Release Of Impounded Property', // Coul'd not find a useful translation
+    'repair_bill' => 'Reparaturkosten',
+    'reprocessing_tax' => 'Reprocessing Tax', // Coul'd not find a useful translation
+    'researching_material_productivity' => 'Researching Material Productivity', // Coul'd not find a useful translation
+    'researching_technology' => 'Researching Technology', // Coul'd not find a useful translation
+    'researching_time_productivity' => 'Researching Time Productivity', // Coul'd not find a useful translation
+    'resource_wars_reward' => 'Resource Wars Reward', // Coul'd not find a useful translation
+    'reverse_engineering' => 'Reverse Engineering', // Coul'd not find a useful translation
+    'security_processing_fee' => 'Security Processing Fee', // Coul'd not find a useful translation
+    'shares' => 'Aktien',
+    'skill_purchase' => 'Skill Purchase', // Coul'd not find a useful translation
+    'sovereignity_bill' => 'Sovereignty Bill', // Coul'd not find a useful translation
+    'store_purchase' => 'Store Purchase', // Coul'd not find a useful translation
+    'store_purchase_refund' => 'Store Purchase Refund', // Coul'd not find a useful translation
+    'structure_gate_jump' => 'Structure Gate Jump', // Coul'd not find a useful translation
+    'transaction_tax' => 'Transaction Tax', // Coul'd not find a useful translation
+    'upkeep_adjustment_fee' => 'Upkeep Adjustment Fee', // Coul'd not find a useful translation
+    'war_ally_contract' => 'War Ally Contract', // Coul'd not find a useful translation
+    'war_fee' => 'War Fee', // Coul'd not find a useful translation
+    'war_fee_surrender' => 'War Fee Surrender', // Coul'd not find a useful translation
+];
