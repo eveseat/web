4d4dd02119ff312135c2401831af4612dc2144b9
Ben Thompson <ben@benthompson.id.au>
2020-02-19 10:45:47 +0000

    fix(notifications): fix datatables error on character notifications from issue passing notification text from array to string

--- src/resources/views/character/notification.blade.php
+++ src/resources/views/character/notification.blade.php
@@ -1,3 +1,3 @@
-<i class="fa fa-comment" data-widget="popover" data-placement="top" title="" data-html="true"
-   data-trigger="hover" data-content="{{ clean_ccp_html($row->text) }}"></i>
+<i class="fa fa-comment" data-toggle="tooltip" data-placement="top" data-html="true"
+   title="{{ clean_ccp_html($row->getOriginal('text')) }}"></i>
 {{ preg_replace('/([a-z0-9])([A-Z])/', '$1 $2', $row->type) }}
