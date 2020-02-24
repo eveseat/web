dba3d2d06189c7d32517d243732a71dfbf8c95fd
Loïc LEUILLIOT <loic.leuilliot@gmail.com>
2020-02-24 21:02:26 +0100

    fix(squads): add missing update form

---
+++ src/Http/Validation/Squad.php
@@ -0,0 +1,47 @@
+<?php
+
+/*
+ * This file is part of SeAT
+ *
+ * Copyright (C) 2015, 2016, 2017, 2018, 2019, 2020  Leon Jacobs
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along
+ * with this program; if not, write to the Free Software Foundation, Inc.,
+ * 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+ */
+
+namespace Seat\Web\Http\Validation;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+/**
+ * Class Squad.
+ *
+ * @package Seat\Web\Http\Validation
+ */
+class Squad extends FormRequest
+{
+    /**
+     * @return array
+     */
+    public function rules()
+    {
+        return [
+            'name'        => 'required|unique:squads|max:255',
+            'type'        => 'required|in:manual,auto,hidden',
+            'description' => 'required',
+            'logo'        => 'mimes:jpeg,jpg,png|max:2000',
+            'filters'     => 'json',
+        ];
+    }
+}
