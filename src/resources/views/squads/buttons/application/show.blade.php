ffb02cf41e48946be88daaeb1c141f99ebaa9675
C Edwards <craig.t.edwards@googlemail.com>
2020-03-06 19:05:42 +0000

    fix(squads): address typo on applications routes (#388)
    a few s's missing and a show

--- src/resources/views/squads/buttons/application/show.blade.php
+++ src/resources/views/squads/buttons/application/show.blade.php
@@ -1,3 +1,3 @@
-<button data-toggle="modal" data-target="#application-read" class="btn btn-sm btn-light" data-url="{{ route('squads.application', $row->application_id) }}">
+<button data-toggle="modal" data-target="#application-read" class="btn btn-sm btn-light" data-url="{{ route('squads.applications.show', $row->application_id) }}">
   <i class="fas fa-eye"></i> {{ trans('web::squads.show') }}
 </button>
