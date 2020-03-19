6128fc850a1450916c20c9f62d6674f6f33b6907
Justin Mercer <darkflame9013@gmail.com>
2020-03-19 11:26:47 -0700

    fix(configuration):  Fix User Management characters display and sort (#384)
    * fix(configuration):  Fix User Management characters display and sort
    
    Co-authored-by: Justin Mercer <justin.mercer9013@gmail.com>

--- src/resources/views/configuration/users/partials/character.blade.php
+++ src/resources/views/configuration/users/partials/character.blade.php
@@ -1,4 +1,4 @@
-<a href="{{ route('character.view.default', ['character_id' => $character->character_id]) }}"
-   data-toggle="tooltip" title="{{ $character->name }}">
+<a href="{{ route('character.view.default', ['character_id' => $character->character_id]) }}">
   {!! img('characters', 'portrait', $character->character_id, 64, ['class' => 'img-circle eve-icon small-icon'], false) !!}
+  {{ $character->name }}
 </a>
