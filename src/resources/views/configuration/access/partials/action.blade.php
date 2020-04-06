31ea21430bd454516ddedf482acc0c69a4a4fde4
Loïc LEUILLIOT <loic.leuilliot@gmail.com>
2020-04-06 14:18:00 +0200

    feat(acl): switch roles list to data-table pattern

---
+++ src/resources/views/configuration/access/partials/action.blade.php
@@ -0,0 +1,17 @@
+<form method="post" action="{{ route('configuration.access.roles.delete', [$row->id]) }}">
+  {{ csrf_field() }}
+  {{ method_field('DELETE') }}
+  <div class="btn-group btn-group-sm float-right">
+    <a href="{{ route('configuration.access.roles.edit', [$row->id]) }}" type="button"
+       class="btn btn-warning">
+      <i class="fas fa-pencil-alt"></i>
+      {{ trans('web::seat.edit') }}
+    </a>
+    @if($row->title !== 'Superuser')
+      <button type="submit" class="btn btn-danger">
+        <i class="fas fa-trash-alt"></i>
+        {{ trans('web::seat.delete') }}
+      </button>
+    @endif
+  </div>
+</form>
