3da251e2c60ab55a63781689e6406ac1a85a0338
Loïc LEUILLIOT <loic.leuilliot@gmail.com>
2020-04-13 15:41:35 +0200

    refactor(assets): implement new corporation asset page

---
+++ src/Http/DataTables/Corporation/Columns/AssetStationColumn.php
@@ -0,0 +1,97 @@
+<?php
+
+/**
+ * This file is part of SeAT
+ *
+ * Copyright (C) 2015 to 2020 Leon Jacobs
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along
+ * with this program; if not, write to the Free Software Foundation, Inc.,
+ * 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+ */
+
+namespace Seat\Web\Http\DataTables\Corporation\Columns;
+
+use Seat\Eveapi\Models\Assets\CorporationAsset;
+use Seat\Web\Http\DataTables\Common\IColumn;
+
+/**
+ * Class AssetStationColumn.
+ *
+ * @package Seat\Web\Http\DataTables\Corporation\Columns
+ */
+class AssetStationColumn implements IColumn
+{
+    /**
+     * Define the station column of a DataTable.
+     *
+     * @param $row
+     * @return string
+     */
+    public function __invoke($row)
+    {
+        if ($row->location_type == 'station')
+            return $row->station->name;
+
+        // in case item has CorpSAG flag, this mean it is inside root division level
+        //
+        // alternatively, stuff can be come from an office and being in safety
+        // in such case, we move to the station relation instead the parent container
+        if (in_array($row->location_flag, ['CorpSAG1', 'CorpSAG2', 'CorpSAG3', 'CorpSAG4', 'CorpSAG5', 'CorpSAG6', 'CorpSAG7', 'CorpDeliveries']))
+            return $this->getLocationNameFromDivision($row);
+
+        if ($row->location_type == 'other' && in_array($row->location_flag, ['AutoFit', 'Unlocked'])) {
+
+            // in case item has CorpSAG flag, this mean it is inside root division level
+            //
+            // alternatively, stuff can be come from an office and being in safety
+            // in such case, we move to the station relation instead the parent container
+            if (in_array($row->container->location_flag, ['CorpSAG1', 'CorpSAG2', 'CorpSAG3', 'CorpSAG4', 'CorpSAG5', 'CorpSAG6', 'CorpSAG7']))
+                return $this->getLocationNameFromDivision($row->container);
+
+            if (in_array($row->container->location_flag, ['CorpDeliveries', 'AssetSafety']))
+                return $row->container->station->name;
+        }
+
+        return '';
+    }
+
+    /**
+     * Return station or structure name from an asset which is inside a division.
+     *
+     * @param \Seat\Eveapi\Models\Assets\CorporationAsset $item
+     * @return string
+     */
+    private function getLocationNameFromDivision(CorporationAsset $item)
+    {
+        // item > division > office > station / structure
+        if (in_array($item->container->location_flag, ['Impounded', 'OfficeFolder'])) {
+            if ($item->container->location_type == 'station')
+                return $item->container->station->name;
+
+            if ($item->container->location_flag == 'Impounded' && $item->container->location_type == 'other')
+                return $item->container->structure->name;
+
+            return $item->container->container->name;
+        }
+
+        // item > division > safety > station
+        if ($item->container->location_flag == 'AssetSafety')
+            return $item->container->station->name;
+
+        if ($item->location_flag == 'CorpDeliveries' && $item->location_type == 'other')
+            return $item->structure->name;
+
+        return '';
+    }
+}
---
+++ src/resources/views/common/assets/buttons/cargo.blade.php
@@ -0,0 +1,3 @@
+<button class="btn btn-sm btn-link" data-toggle="modal" data-target="#container-detail" data-url="">
+    <i class="fas fa-cubes"></i>
+</button>
