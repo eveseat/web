d0e957ad226a6f56cb656e5712614a27d680c774
Loïc LEUILLIOT <loic.leuilliot@gmail.com>
2020-04-13 16:50:43 +0200

    refactor(assets): add fitting from ship in assets

--- src/resources/views/common/assets/buttons/fitting.blade.php
+++ src/resources/views/common/assets/buttons/fitting.blade.php
@@ -1,3 +1,3 @@
-<button class="btn btn-sm btn-link" data-toggle="modal" data-target="#fitting-detail" data-url="">
+<button class="btn btn-sm btn-link" data-toggle="modal" data-target="#fitting-detail" data-url="{{ route('corporation.view.assets.fitting', ['corporation_id' => $row->corporation_id, 'item_id' => $row->item_id]) }}">
     <i class="fas fa-wrench"></i>
 </button>
