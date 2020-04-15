6c5ddeb0ce4351090bc9ce62981850c7bd811291
Loïc LEUILLIOT <loic.leuilliot@gmail.com>
2020-04-15 11:35:21 +0200

    chore: bump version to avoid miss-leading analytics

--- src/Config/web.config.php
+++ src/Config/web.config.php
@@ -21,6 +21,6 @@
  */
 
 return [
-    'version'                  => '4.0.0-dev',
+    'version'                  => '4.0.x-dev',
     'queue_status_update_time' => 10 * 1000, // milliseconds = seconds x 1,000
 ];
