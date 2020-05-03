3474bcad8a990cb50335ce650f5e08797af3ba45
Ben Thompson <ben@benthompson.id.au>
2020-05-03 12:45:08 +0000

    UI enhancement: add progress bar to industry jobs

---
+++ src/resources/views/common/industries/partials/progress.blade.php
@@ -0,0 +1,4 @@
+<td>
+    <div class="countdown-progressbar" data-expiry-time="{{$row->end_date}}"
+        data-install-time="{{$row->start_date}}"></div>
+</td>
--- src/resources/views/corporation/industry.blade.php
+++ src/resources/views/corporation/industry.blade.php
@@ -22,4 +22,49 @@
 
 @push('javascript')
   {!! $dataTable->scripts() !!}
+
+  <script>
+  function updateProgressBar() {
+    $(".countdown-progressbar").each(function () {
+      var expiry_time = moment.utc($(this).attr('data-expiry-time'), "YYYY-MM-DD hh:mm:ss").valueOf();
+      var install_time = moment.utc($(this).attr('data-install-time'), "YYYY-MM-DD hh:mm:ss").valueOf();
+      var percentage_complete = (moment.utc() - install_time) / (expiry_time - install_time);
+      var percentage_rounded = (Math.round(percentage_complete * 100) / 100);
+      var progress_value = percentage_rounded * 100;
+      var progress_class = 'progress-bar progress-bar-striped';
+
+      // ensure progress-bar will not exceed 100%
+      if (progress_value > 100)
+          progress_value = 100;
+
+      // design progress-bar according to completion
+      switch (true) {
+          case (percentage_rounded < 0.4):
+              progress_class += ' progress-bar-success progress-bar-striped';
+              break;
+          case (percentage_rounded >= 0.4 && percentage_rounded < 0.6):
+              progress_class += ' progress-bar-info progress-bar-striped';
+              break;
+          case (percentage_rounded >= 0.6 && percentage_rounded < 0.8):
+              progress_class += ' progress-bar-warning progress-bar-striped';
+              break;
+          case (percentage_rounded < 1):
+              progress_class += ' progress-bar-danger progress-bar-striped';
+              break;
+          default:
+              progress_class += ' progress-bar-danger progress-bar-striped';
+      }
+
+      // draw the updated progress bar
+      $(this).html(
+          "<div class='progress active'>" +
+          '<div class="' + progress_class + '" role="progressbar" ' +
+          'aria-valuenow="' + progress_value + '" aria-valuemin="0" aria-valuemax="100" ' +
+          'style="width: ' + progress_value + '%" >' + progress_value +
+          '%</div>' +
+          '</div>'
+      );
+    });
+  }
+  </script>
 @endpush
