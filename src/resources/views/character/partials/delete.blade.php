d4199ec8b39238a0205e8297ca48ceb6ec618f3b
Loïc LEUILLIOT <loic.leuilliot@gmail.com>
2020-06-04 21:54:07 +0200

    fix: send proper parameter to routes

--- src/resources/views/character/partials/delete.blade.php
+++ src/resources/views/character/partials/delete.blade.php
@@ -1,5 +1,5 @@
 @can('global.superuser')
-  <a class="btn btn-danger btn-sm confirmlink" href="{{ route('character.delete', ['character_id' => $row->character_id]) }}">
+  <a class="btn btn-danger btn-sm confirmlink" href="{{ route('character.delete', ['character' => $row->character_id]) }}">
     {{ trans('web::seat.delete') }}
   </a>
 @endcan
