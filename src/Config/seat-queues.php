a13a23b50a3d1c3e34c0d1715456c12912d44d62
Loïc LEUILLIOT <loic.leuilliot@gmail.com>
2020-06-13 22:34:21 +0200

    feat: ensure queue configuration is customizable
    remove hardcode related to horizon pool configuration in web provider and use a dynamic configuration file that will be merged in proper place.

---
+++ src/Config/seat-queues.php
@@ -0,0 +1,39 @@
+<?php
+
+/*
+ * This file is part of SeAT
+ *
+ * Copyright (C) 2015 to 2020 Leon Jacobs
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along
+ * with this program; if not, write to the Free Software Foundation, Inc.,
+ * 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+ */
+
+return [
+    'pool-1' => [
+        'connection'   => 'redis',
+        'queue'        => ['default', 'high', 'characters', 'corporations', 'public', 'notifications'],
+        'balance'      => 'auto', // valid values are simple, auto or false
+        'tries'        => 3,
+        'timeout'      => 900, // 15 minutes,
+        // the minimum workers which must be spawn per queue in the pool - in auto balancing mode
+        // this is the worker amount set on an idle queue
+        // this value must be greater than 0
+        'minProcesses' => 1,
+        // the maximum workers which must be spawned by the pool - in auto balancing mode
+        'maxProcesses' => (int) env('QUEUE_WORKERS', 2),
+        // the processes which must be assigned - in simple mode
+        // 'processes'    => (int) env('QUEUE_WORKERS', 6),
+    ],
+];
