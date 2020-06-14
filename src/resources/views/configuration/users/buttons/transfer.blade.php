4fd1c49eada60aef60f0a842a17fe71eda2583d6
Loïc Leuilliot <loic.leuilliot@gmail.com>
2020-06-14 13:47:08 +0200

    feat(users): restore ability to reassign a character to another account (#445)
    when a character is transferred across accounts, we logs an event into security logs which will keep a track of action.

---
+++ src/resources/views/configuration/users/buttons/transfer.blade.php
@@ -0,0 +1,3 @@
+<button type="button" class="btn btn-link" data-toggle="modal" data-target="#character-transfer-modal" data-character="{{ $character->character_id }}" data-user="{{ $user->id }}">
+  <i class="fas fa-random"></i> Transfer
+</button>
