3a18fc7cf370459e0dbc3730e674162b52c69782
Loïc LEUILLIOT <loic.leuilliot@gmail.com>
2020-06-20 17:01:21 +0200

    test(acl): add security logs table

--- tests/database/migrations/2020_05_09_151051_create_character_roles_test_table.php
+++ tests/database/migrations/2020_05_09_151051_create_character_roles_test_table.php
@@ -22,7 +22,11 @@
 
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
 
+/**
+ * Class CreateCharacterRolesTestTable.
+ */
 class CreateCharacterRolesTestTable extends Migration
 {
     /**
