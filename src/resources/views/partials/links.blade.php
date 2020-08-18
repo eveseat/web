67e37af301752ba7d6793537e7f33a61bea078eb
Loïc LEUILLIOT <loic.leuilliot@gmail.com>
2020-08-18 21:47:00 +0200

    fix(contacts): update entity links to third party platform
    zKillboard is requiring a trailing slash
    evewho switched from entity name to entity ID
    
    Closes eveseat/seat#631

--- src/resources/views/partials/links.blade.php
+++ src/resources/views/partials/links.blade.php
@@ -10,24 +10,12 @@
     <img src="{{ asset('web/img/evesearch.png') }}">
   </a>
 @endif
-@if ($type == 'corporation')
-  <a href="http://evewho.com/corp/{{ $id }}"
-     target="_blank" class="id-to-name" data-id="{{$id}}">
-    <img src="{{ asset('web/img/evewho.png') }}">
-  </a>
-@elseif ($type == 'alliance')
-  <a href="http://evewho.com/alli/{{ $id }}"
-     target="_blank" class="id-to-name" data-id="{{$id}}">
-    <img src="{{ asset('web/img/evewho.png') }}">
-  </a>
-@else
-  <a href="http://evewho.com/pilot/{{ $id }}"
-     target="_blank" class="id-to-name" data-id="{{$id}}">
-    <img src="{{ asset('web/img/evewho.png') }}">
-  </a>
-@endif
 @if (in_array($type, ['corporation', 'alliance', 'character']))
-  <a href="https://zkillboard.com/{{ $type }}/{{ $id }}"
+  <a href="https://evewho.com/{{ $type }}/{{ $id }}"
+     target="_blank">
+    <img src="{{ asset('web/img/evewho.png') }}">
+  </a>
+  <a href="https://zkillboard.com/{{ $type }}/{{ $id }}/"
      target="_blank">
     <img src="{{ asset('web/img/zkillboard.png') }}">
   </a>
