4777bf2d7e7104adec05170921216b13b9ce2c71
Loïc Leuilliot <loic.leuilliot@gmail.com>
2020-08-31 18:21:28 +0200

    build(core): version bump

--- src/Config/web.config.php
+++ src/Config/web.config.php
@@ -21,6 +21,6 @@
  */
 
 return [
-    'version'                  => '4.2.0',
+    'version'                  => '4.2.1',
     'queue_status_update_time' => 10 * 1000, // milliseconds = seconds x 1,000
 ];
