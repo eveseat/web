6a48b6197a1a96aad02844fe2fed85a163ee5c92
Loïc LEUILLIOT <loic.leuilliot@gmail.com>
2020-09-27 21:32:33 +0200

    refactor: remove entity name from redis cache

--- src/resources/views/partials/alliance.blade.php
+++ src/resources/views/partials/alliance.blade.php
@@ -1,8 +1,9 @@
 @if(! is_null($alliance) && (! is_null($alliance->entity_id) || ! is_null($alliance->alliance_id)))
-  {!! img('alliances', 'logo', $alliance->alliance_id ?? $alliance->entity_id, 32, ['class' => 'img-circle eve-icon small-icon'], false) !!}
-  {!!
-    cache(sprintf('name_id:%s', $alliance->alliance_id ?? $alliance->entity_id), function () use ($alliance) {
-      return sprintf('<span class="id-to-name" data-id="%d">%s</span>', $alliance->alliance_id ?? $alliance->entity_id, trans('web::seat.unknown'));
-    })
-  !!}
+  @if($alliance->name)
+      {!! img('alliances', 'logo', $alliance->alliance_id ?? $alliance->entity_id, 32, ['class' => 'img-circle eve-icon small-icon'], false) !!}
+      <span>{{ $alliance->name }}</span>
+  @else
+    {!! img('alliances', 'logo', $alliance->alliance_id ?? $alliance->entity_id, 32, ['class' => 'img-circle eve-icon small-icon'], false) !!}
+    <span class="id-to-name" data-id="{{ $alliance->alliance_id ?? $alliance->entity_id }}">{{ trans('web::seat.unknown') }}</span>
+  @endif
 @endif
--- src/resources/views/partials/character.blade.php
+++ src/resources/views/partials/character.blade.php
@@ -1,13 +1,16 @@
 @if ($character->name && $character->name !== trans('web::seat.unknown'))
-  <a href="{{ route('character.view.default', ['character' => $character->character_id ?? $character->entity_id]) }}">
-    {!! img('characters', 'portrait', $character->character_id ?? $character->entity_id, 32, ['class' => 'img-circle eve-icon small-icon'], false) !!}
-    {{ $character->name }}
-  </a>
+  @if(\Seat\Eveapi\Models\Character\CharacterInfo::find($character->character_id ?? $character->entity_id))
+    <a href="{{ route('character.view.default', ['character' => $character->character_id ?? $character->entity_id]) }}">
+      {!! img('characters', 'portrait', $character->character_id ?? $character->entity_id, 32, ['class' => 'img-circle eve-icon small-icon'], false) !!}
+      {{ $character->name }}
+    </a>
+  @else
+    <span>
+      {!! img('characters', 'portrait', $character->character_id ?? $character->entity_id, 32, ['class' => 'img-circle eve-icon small-icon'], false) !!}
+      {{ $character->name }}
+    </span>
+  @endif
 @else
   {!! img('characters', 'portrait', $character->character_id ?? $character->entity_id, 32, ['class' => 'img-circle eve-icon small-icon'], false) !!}
-  {!!
-    cache(sprintf('name_id:%s', $character->character_id ?? $character->entity_id), function () use ($character) {
-      return sprintf('<span class="id-to-name" data-id="%d">%s</span>', $character->character_id ?? $character->entity_id, trans('web::seat.unknown'));
-    })
-  !!}
+  <span class="id-to-name" data-id="{{ $character->character_id ?? $character->entity_id }}">{{ trans('web::seat.unknown') }}</span>
 @endif
