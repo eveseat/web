fc82db87708f652adf5d4fd1451afdb3c39d76b8
Loïc Leuilliot <loic.leuilliot@gmail.com>
2020-12-12 12:04:06 +0100

    refactor: use new generic token status indicator (#523)
    same token status indicator is used for character summary and corporation tracking.
    it is also added to the characters list.
    corporation member tracking has been revamp to match with new design and benefits of the dynamic filter system.
    
    Closes eveseat/seat#726

--- src/Http/Controllers/Corporation/TrackingController.php
+++ src/Http/Controllers/Corporation/TrackingController.php
@@ -22,14 +22,11 @@
 
 namespace Seat\Web\Http\Controllers\Corporation;
 
-use Illuminate\Database\Eloquent\Builder;
 use Seat\Eveapi\Models\Corporation\CorporationInfo;
-use Seat\Eveapi\Models\Corporation\CorporationMemberTracking;
-use Seat\Eveapi\Models\Sde\SolarSystem;
-use Seat\Eveapi\Models\Sde\StaStation;
-use Seat\Eveapi\Models\Universe\UniverseStructure;
 use Seat\Web\Http\Controllers\Controller;
-use Yajra\DataTables\DataTables;
+use Seat\Web\Http\DataTables\Corporation\Intel\MembersTrackingDataTable;
+use Seat\Web\Http\DataTables\Scopes\CorporationScope;
+use Seat\Web\Http\DataTables\Scopes\Filters\TokenStatusScope;
 
 /**
  * Class TrackingController.
@@ -40,105 +37,14 @@ class TrackingController extends Controller
 {
     /**
      * @param \Seat\Eveapi\Models\Corporation\CorporationInfo $corporation
+     * @param \Seat\Web\Http\DataTables\Corporation\Intel\MembersTrackingDataTable $dataTable
      *
      * @return \Illuminate\Contracts\View\Factory|\Illuminate\View\View
      */
-    public function getTracking(CorporationInfo $corporation)
+    public function getTracking(CorporationInfo $corporation, MembersTrackingDataTable $dataTable)
     {
-        return view('web::corporation.tracking', compact('corporation'));
-    }
-
-    /**
-     * @param \Seat\Eveapi\Models\Corporation\CorporationInfo $corporation
-     *
-     * @return mixed
-     * @throws \Exception
-     */
-    public function getMemberTracking(CorporationInfo $corporation)
-    {
-        $selected_status = collect(request('selected_refresh_token_status'));
-
-        $tracking = $this->getCorporationMemberTracking($corporation->corporation_id);
-
-        if($selected_status->contains('valid_token'))
-            $tracking->has('refresh_token');
-
-        if($selected_status->contains('invalid_token'))
-            $tracking->doesntHave('refresh_token');
-
-        return DataTables::of($tracking)
-            ->editColumn('refresh_token.user.main_character.name', function ($row) {
-                if(is_null(optional($row)->refresh_token))
-                    return '';
-
-                return view('web::partials.character', ['character' => $row->refresh_token->user->main_character])->render();
-            })
-            ->editColumn('character.name', function ($row) {
-                return view('web::partials.character', ['character' => $row->character])->render();
-            })
-            ->addColumn('location', function ($row) {
-                return view('web::corporation.partials.location', compact('row'))->render();
-            })
-            ->editColumn('ship.typeName', function ($row) {
-                if(is_null(optional($row)->ship))
-                    return '';
-
-                return view('web::partials.type', ['type_id' => $row->ship->typeID, 'type_name' => $row->ship->typeName])->render();
-            })
-            ->addColumn('refresh_token_status', function ($row) {
-
-                $refresh_token = false;
-
-                if(! is_null(optional($row)->refresh_token))
-                    $refresh_token = true;
-
-                return view('web::corporation.partials.refresh-token', compact('refresh_token'))->render();
-            })
-            ->filterColumn('location', function ($query, $keyword) {
-
-                $system_ids = collect();
-
-                if(strlen($keyword) > 1) {
-                    $system_ids = SolarSystem::where('name', 'like', '%' . $keyword . '%')
-                        ->select('system_id')
-                        ->get()
-                        ->map(function ($system) {
-                            return $system->system_id;
-                        });
-                }
-
-                $station_ids = StaStation::where('stationName', 'like', '%' . $keyword . '%')->get()->map(function ($station) { return $station->stationID; });
-                $structure_ids = UniverseStructure::where('name', 'like', '%' . $keyword . '%')->get()->map(function ($structure) { return $structure->structure_id; });
-
-                $query->whereIn('location_id', array_merge($system_ids->toArray(), $station_ids->toArray(), $structure_ids->toArray()));
-
-            })
-            ->rawColumns([
-                'refresh_token.user.main_character.name', 'character.name', 'location',
-                'ship.typeName', 'refresh_token_status',
-            ])
-            ->make(true);
-
-    }
-
-    /**
-     * Return the Member Tracking for a Corporation.
-     *
-     * @param int $corporation_id
-     *
-     * @return \Illuminate\Database\Eloquent\Builder
-     */
-    private function getCorporationMemberTracking(int $corporation_id): Builder
-    {
-
-        return CorporationMemberTracking::with(
-            'refresh_token',
-            'refresh_token.user',
-            'refresh_token.user.main_character',
-            'ship',
-            'character'
-        )
-            ->select('corporation_member_trackings.*')
-            ->where('corporation_id', $corporation_id);
+        return $dataTable->addScope(new CorporationScope('corporation.tracking', [$corporation->corporation_id]))
+            ->addScope(new TokenStatusScope(request()->input('filters.type')))
+            ->render('web::corporation.tracking', compact('corporation'));
     }
 }
---
+++ src/Http/DataTables/Corporation/Intel/MembersTrackingDataTable.php
@@ -0,0 +1,149 @@
+<?php
+
+/*
+ * This file is part of SeAT
+ *
+ * Copyright (C) 2015 to 2020 Leon Jacobs
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along
+ * with this program; if not, write to the Free Software Foundation, Inc.,
+ * 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+ */
+
+namespace Seat\Web\Http\DataTables\Corporation\Intel;
+
+use Seat\Eveapi\Models\Corporation\CorporationMemberTracking;
+use Seat\Eveapi\Models\Sde\SolarSystem;
+use Seat\Eveapi\Models\Sde\StaStation;
+use Seat\Eveapi\Models\Universe\UniverseStructure;
+use Seat\Web\Models\User;
+use Yajra\DataTables\Services\DataTable;
+
+/**
+ * Class MembersTrackingDataTable.
+ *
+ * @package Seat\Web\Http\DataTables\Corporation\Intel
+ */
+class MembersTrackingDataTable extends DataTable
+{
+    /**
+     * @return \Illuminate\Http\JsonResponse
+     * @throws \Exception
+     */
+    public function ajax()
+    {
+        return datatables()
+            ->eloquent($this->applyScopes($this->query()))
+            ->editColumn('refresh_token.expires_on', function ($row) {
+                return view('web::character.partials.token_status', ['refresh_token' => $row->refresh_token])
+                    ->render();
+            })
+            ->editColumn('character.name', function ($row) {
+                return view('web::partials.character', ['character' => $row->character])->render();
+            })
+            ->editColumn('ship.typeName', function ($row) {
+                return view('web::partials.type', ['type_id' => $row->ship->typeID, 'type_name' => $row->ship->typeName])
+                    ->render();
+            })
+            ->editColumn('start_date', function ($row) {
+                return view('web::partials.date', ['datetime' => $row->start_date])->render();
+            })
+            ->editColumn('logon_date', function ($row) {
+                return view('web::partials.date', ['datetime' => $row->logon_date])->render();
+            })
+            ->addColumn('location', function ($row) {
+                return $row->location->name ?? trans('web::seat.unknown');
+            })
+            ->filterColumn('location', function ($query, $keyword) {
+                $system_ids = collect();
+
+                if(strlen($keyword) > 1) {
+                    $system_ids = SolarSystem::where('name', 'like', '%' . $keyword . '%')
+                        ->select('system_id')
+                        ->get()
+                        ->map(function ($system) {
+                            return $system->system_id;
+                        });
+                }
+
+                $station_ids = StaStation::where('stationName', 'like', '%' . $keyword . '%')->get()
+                    ->map(function ($station) { return $station->stationID; });
+                $structure_ids = UniverseStructure::where('name', 'like', '%' . $keyword . '%')->get()
+                    ->map(function ($structure) { return $structure->structure_id; });
+
+                $location_ids = array_merge($system_ids->toArray(), $station_ids->toArray(), $structure_ids->toArray());
+
+                $query->whereIn('location_id', $location_ids);
+            })
+            ->filterColumn('character.name', function ($query, $keyword) {
+                $characters = collect();
+
+                // in case authenticated user is an admin, grant him search by main character
+                // we prevent that general feature since a main can be outside a corporation
+                // and therefore, a ceo, director or anything else than admin might not be granted
+                // to get access to such information.
+                if (auth()->user()->isAdmin()) {
+                    $characters = User::where('name', 'like', "%{$keyword}%")->get()
+                        ->map(function ($user) {
+                            return $user->characters->pluck('character_id');
+                        });
+                }
+
+                $query->whereIn('character_id', $characters->toArray())
+                    ->orWhereHas('character', function ($query) use ($keyword) {
+                        $query->where('name', 'like', "%{$keyword}%");
+                    });
+            })
+            ->rawColumns(['refresh_token.expires_on', 'character.name', 'ship.typeName', 'start_date', 'logon_date'])
+            ->make(true);
+    }
+
+    /**
+     * @return \Yajra\DataTables\Html\Builder
+     */
+    public function html()
+    {
+        return $this->builder()
+            ->postAjax([
+                'data' => 'function(d) { d.filters = {}; $("[data-filter-field].dt-filters.active").each(function (i, e) { var a = $(e); var field = a.data("filter-field"); var value = a.data("filter-value"); if (! d.filters[field]) { d.filters[field] = []; } d.filters[field].push(value); }); }',
+            ])
+            ->columns($this->getColumns())
+            ->orderBy(1, 'asc')
+            ->parameters([
+                'drawCallback' => 'function() { ids_to_names(); $("[data-toggle=tooltip]").tooltip(); }',
+            ]);
+    }
+
+    /**
+     * @return \Illuminate\Database\Eloquent\Builder
+     */
+    public function query()
+    {
+        return CorporationMemberTracking::with('character', 'refresh_token', 'ship');
+    }
+
+    /**
+     * @return array
+     */
+    public function getColumns()
+    {
+        return [
+            ['data' => 'refresh_token.expires_on', 'title' => trans('web::seat.token'), 'orderable' => false, 'searchable' => false],
+            ['data' => 'character.name', 'title' => trans_choice('web::seat.name', 1)],
+            ['data' => 'location', 'title' => trans('web::seat.location'), 'orderable' => false],
+            ['data' => 'ship.typeName', 'title' => trans('web::seat.current_ship')],
+            ['data' => 'start_date', 'title' => trans('web::seat.joined')],
+            ['data' => 'logon_date', 'title' => trans('web::seat.last_login')],
+        ];
+    }
+}
