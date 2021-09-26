e6d0ea3178f1c178f86f8bf62d3fa5d9a959f2e4
Crypta Eve <crypta@crypta.tech>
2021-09-26 09:54:07 +0000

    fix: rearrange buttons extractions

--- src/resources/views/corporation/extraction/extraction.blade.php
+++ src/resources/views/corporation/extraction/extraction.blade.php
@@ -6,19 +6,20 @@
 
 <p class="container-fluid">
 <div class="btn-toolbar">
-  <div class="btn-group">
-    <button class="btn btn-primary" type="button" data-toggle="collapse" data-target=".rate-collapse" >Toggle Ore Rate</button>
-  </div>
-
   @can('moon.manage_moon_reports')
-  <div class="btn-group pl-3">
-
-      <button type="button" data-toggle="modal" data-target="#moon-import" class="btn btn-primary float-right" aria-label="Settings">
-        <i class="fas fa-cogs"></i> Add Missing Moon Scan
+  <div class="btn-group pr-3">
+      <button type="button" data-toggle="modal" data-target="#moon-import" class="btn btn-success float-right" aria-label="Settings">
+        <i class="fas fa-plus-square pr-1"></i>Add Missing Moon Scan
       </button>
-
   </div>
   @endcan
+
+
+  <div class="btn-group">
+    <button class="btn btn-primary" type="button" data-toggle="collapse" data-target=".rate-collapse" >
+      <i class="fas fa-eye pr-1"></i>Toggle Ore Rate
+    </button>
+  </div>
 </div>
 </p>
 
