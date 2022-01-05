b20f762bee6ac434587fea15085bc88adc4b0c40
Loïc LEUILLIOT <loic.leuilliot@gmail.com>
2022-01-05 09:37:13 +0100

    fix: use proper favicons path

--- src/resources/views/includes/favico.blade.php
+++ src/resources/views/includes/favico.blade.php
@@ -8,6 +8,7 @@
 <link rel="apple-touch-icon" sizes="152x152" href="{{ asset('web/img/favico/apple-icon-152x152.png') }}">
 <link rel="apple-touch-icon" sizes="180x180" href="{{ asset('web/img/favico/apple-icon-180x180.png') }}">
 <link rel="icon" type="image/png" sizes="192x192" href="{{ asset('web/img/favico/android-icon-192x192.png') }}">
+<link rel="icon" type="image/png" sizes="144x144" href="{{ asset('web/img/favico/android-icon-144x144.png') }}">
 <link rel="icon" type="image/png" sizes="32x32" href="{{ asset('web/img/favico/favicon-32x32.png') }}">
 <link rel="icon" type="image/png" sizes="96x96" href="{{ asset('web/img/favico/favicon-96x96.png') }}">
 <link rel="icon" type="image/png" sizes="16x16" href="{{ asset('web/img/favico/favicon-16x16.png') }}">
