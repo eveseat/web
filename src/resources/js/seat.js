2648af0d8007d15dc3c90d9f24abb02f45617f3b
Loïc LEUILLIOT <loic.leuilliot@gmail.com>
2022-01-25 13:23:36 +0100

    fix(ui): remove popovers and tooltips init since it's done by tabler

--- src/resources/js/seat.js
+++ src/resources/js/seat.js
@@ -68,18 +68,6 @@ $(document).ready(function () {
     $("img").unveil(100);
 });
 
-// Enable bootstrap popovers
-var popoverTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="popover"]'))
-var popoverList = popoverTriggerList.map(function (popoverTriggerEl) {
-    return new bootstrap.Popover(popoverTriggerEl);
-});
-
-// Enable bootstrap tooltips
-var tooltipTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="tooltip"]'))
-var tooltipList = tooltipTriggerList.map(function (tooltipTriggerEl) {
-    return new bootstrap.Tooltip(tooltipTriggerEl)
-})
-
 // Initialize DataTables on <table> tags
 // with the 'datatable' class.
 //
