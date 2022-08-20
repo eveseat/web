dca1eefc908c17a0b2ee1f59759417b316a5facb
Leon Jacobs <leonja511@gmail.com>
2022-08-20 11:42:10 +0200

    New translations mail.php (Korean)

---
+++ src/resources/lang/ko/mail.php
@@ -0,0 +1,32 @@
+<?php
+
+/*
+ * This file is part of SeAT
+ *
+ * Copyright (C) 2015 to 2022 Leon Jacobs
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along
+ * with this program; if not, write to the Free Software Foundation, Inc.,
+ * 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+ */
+
+return [
+    'read'         => 'Read',
+    'date'         => 'Date',
+    'sender'       => 'Sender',
+    'subject'      => 'Subject',
+    'mailing_list' => 'Mailing List',
+    'sent'         => 'Sent',
+    'author'       => 'Author',
+    'recipients'   => 'Recipients',
+];
