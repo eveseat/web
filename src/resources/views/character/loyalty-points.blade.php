f8f3b3d048e900eaedc3dba999e87879283a9512
Loïc LEUILLIOT <loic.leuilliot@gmail.com>
2022-08-21 14:10:22 +0200

    fix(character): ensure lp blade is renderable

--- src/resources/views/character/loyalty-points.blade.php
+++ src/resources/views/character/loyalty-points.blade.php
@@ -1,45 +1,31 @@
-@extends('web::character.layouts.view', ['viewname' => 'loyalty-points', 'breadcrumb' => trans('web::seat.loyalty_points')])
+@extends('web::layouts.character', ['viewname' => 'loyalty-points', 'breadcrumb' => trans('web::seat.loyalty_points')])
 
-@section('page_header', trans_choice('web::seat.character', 1) . ' ' . trans('web::seat.loyalty_points'))
+@section('page_description', trans_choice('web::seat.character', 1) . ' ' . trans('web::seat.loyalty_points'))
 
 @inject('request', 'Illuminate\Http\Request')
 
 @section('character_content')
 
   <div class="card">
-    <div class="card-header">
-      <h3 class="card-title">
-        {{ trans('web::seat.loyalty_points') }}
-      </h3>
-      @if($character->refresh_token)
-      <div class="card-tools">
-        <div class="input-group input-group-sm">
+    <div class="card-header d-flex align-items-center">
+      <div class="col-auto me-5">
+        <h3 class="card-title">{{ trans('web::seat.loyalty_points') }}</h3>
+      </div>
+      <div class="col-6">
+        @include('web::character.includes.dt-character-selector')
+      </div>
+      <div class="ms-auto">
+        @if($character->refresh_token)
           @include('web::components.jobs.buttons.update', ['type' => 'character', 'entity' => $character->character_id, 'job' => 'character.loyalty_points', 'label' => trans('web::seat.update_loyalty_points')])
-        </div>
+        @endif
       </div>
-      @endif
     </div>
-    <div class="card-body">
-      <div class="mb-3">
-        <select multiple="multiple" id="dt-character-selector" class="form-control" style="width: 100%;">
-          @if($character->refresh_token)
-            @foreach($character->refresh_token->user->characters as $character_info)
-              @if($character_info->character_id == $character->character_id)
-                <option selected="selected" value="{{ $character_info->character_id }}">{{ $character_info->name }}</option>
-              @else
-                <option value="{{ $character_info->character_id }}">{{ $character_info->name }}</option>
-              @endif
-            @endforeach
-          @else
-            <option selected="selected" value="{{ $character->character_id }}">{{ $character->name }}</option>
-          @endif
-        </select>
-      </div>
 
-      {{ $dataTable->table() }}
+    {{ $dataTable->table(['class' => 'table card-table table-vcenter table-hover table-striped text-nowrap']) }}
 
-    </div>
   </div>
+
+  @include('web::common.contracts.modals.details.details')
 
 @stop
 
