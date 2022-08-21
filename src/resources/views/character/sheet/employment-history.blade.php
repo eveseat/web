6534834d9d98d941b1e7ff266c5e4c0e86c4428e
Loïc Leuilliot <loic.leuilliot@gmail.com>
2022-08-21 13:50:03 +0200

    ui: improve character sheet (#616)
    - redesign corporation history, showing bigger row and improving scroll integration
    - use ribbon in order to show token state rather than icon

--- src/resources/views/character/sheet/employment-history.blade.php
+++ src/resources/views/character/sheet/employment-history.blade.php
@@ -1,4 +1,4 @@
-<div class="card mb-3">
+<div class="card mb-3" style="height: 28rem">
     <div class="card-header">
         <h3 class="card-title">
             {{ trans('web::seat.employment_history') }}
@@ -9,30 +9,31 @@
             </div>
         @endif
     </div>
-    <div class="card-body overflow-auto" style="max-height: 200px;">
+    <div class="card-body card-body-scrollable card-body-scrollable-shadow">
+        <div class="divide-y">
+            @if($character->corporation_history->isNotEmpty())
+                    @foreach($character->corporation_history as $history)
+                        <div>
+                            <div class="row">
+                                <div class="col-auto">
+                                    <span class="avatar">
+                                        {!! img('corporations', 'logo', $history->corporation_id, 32, ['class' => 'img-circle eve-icon small-icon'], false) !!}
+                                    </span>
+                                </div>
+                                <div class="col">
+                                    <div class="text-truncate">
+                                        <strong class="id-to-name" data-id="{{ $history->corporation_id }}">{{ $history->corporation->name ?? trans('web::seat.unknown') }}</strong>
+                                    </div>
+                                    <div class="text-muted" data-bs-toggle="tooltip" data-bs-placement="left" title="{{ carbon($history->start_date)->toDateString() }}">{{ human_diff($history->start_date) }}</div>
+                                </div>
+                            </div>
+                        </div>
 
-        @if($character->corporation_history->isNotEmpty())
-            <ul class="list-unstyled">
-
-                @foreach($character->corporation_history as $history)
-
-                    <li>
-                        {!! img('corporations', 'logo', $history->corporation_id, 32, ['class' => 'img-circle eve-icon small-icon'], false) !!}
-                        <b><span class="id-to-name"
-                                 data-id="{{ $history->corporation_id }}">{{ trans('web::seat.unknown') }}</span></b>
-                        on {{ carbon($history->start_date)->toDateString() }}
-                        <span class="float-right">
-                 {{ human_diff($history->start_date) }}
-                </span>
-                    </li>
-
-                @endforeach
-
-            </ul>
-        @else
-            {{ trans('web::seat.no_employment_information') }}
-        @endif
-
+                    @endforeach
+            @else
+                {{ trans('web::seat.no_employment_information') }}
+            @endif
+        </div>
     </div>
     <div class="card-footer">
         {{ $character->corporation_history->count() }} {{ trans_choice('web::seat.corporation', $character->corporation_history->count()) }}
