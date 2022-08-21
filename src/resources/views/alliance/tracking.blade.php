bd8f6f05bf69425b890127121a8258e41895a937
Loïc LEUILLIOT <loic.leuilliot@gmail.com>
2022-08-21 11:48:22 +0200

    ui(ally): improve table rendering

--- src/resources/views/alliance/tracking.blade.php
+++ src/resources/views/alliance/tracking.blade.php
@@ -5,22 +5,22 @@
 @section('alliance_content')
 
   <div class="card">
-    <div class="card-header">
-      <h3 class="card-title">{{ trans('web::seat.tracking') }}</h3>
-      @if ($alliance->members->count() > $alliance->corporations->count())
-      <div class="card-tools">
-        <div class="input-group input-group-sm">
-        <span class="text-warning">
-                  <i class="fas fa-exclamation-triangle" data-bs-toggle="tooltip" title="SeAT is missing {{ $alliance->members->count() - $alliance->corporations->count() }} member corporations. These will not appear in the table below."></i>
-                </span>
-        </div>
+    <div class="card-header d-flex align-items-center">
+      <div class="col-auto me-5">
+        <h3 class="card-title">{{ trans('web::seat.tracking') }}</h3>
       </div>
-      @endif
+      <div class="ms-auto">
+        @if ($alliance->members->count() > $alliance->corporations->count())
+          <div class="input-group input-group-sm">
+            <span class="text-warning">
+              <i class="fas fa-exclamation-triangle" data-bs-toggle="tooltip" title="SeAT is missing {{ $alliance->members->count() - $alliance->corporations->count() }} member corporations. These will not appear in the table below."></i>
+            </span>
+          </div>
+        @endif
+      </div>
     </div>
-    <div class="card-body">
 
-      {!! $dataTable->table() !!}
-    </div>
+    {{ $dataTable->table(['class' => 'table card-table table-vcenter table-hover table-striped text-nowrap']) }}
   </div>
 
 @stop
