d7fde6f6b835714428b9e12f935ad6771c5e40c5
Leon Jacobs <leonja511@gmail.com>
2022-08-22 22:07:54 +0200

    New translations about.php (Chinese Simplified)

--- src/resources/lang/zh-CN/about.php
+++ src/resources/lang/zh-CN/about.php
@@ -33,54 +33,50 @@ return [
     // CCP Disclaimer pane
     //
     'disclaimer_pane_title' => 'CCP的免责声明',
-    'disclaimer_pane_content' => ':eve_online and the EVE logo are the
-            registered trademarks of :ccp_enterprise.
-            All rights are reserved worldwide. All other trademarks are the property of their respective owners.
-            EVE Online, the EVE logo, EVE and all associated logos and designs are the intellectual property of CCP hf.
-            All artwork, screenshots, characters, vehicles, storylines, world facts or other recognizable features of
-            the intellectual property relating to these trademarks are likewise the intellectual property of CCP hf.
-            CCP hf. has granted permission to :seat
-            to use EVE Online and all associated logos and designs for promotional and information purposes on
-            its project but does not endorse, and is not in any way affiliated with, SeAT. CCP is in no way responsible
-            for the content on or functioning of this software, nor can it be liable for any damage arising from the use
-            of this system.',
+    'disclaimer_pane_content' => ':eve_online 和 EVE 标识是
+            被注册为 :ccp_enterprise 的商标
+            在全球范围内保留所有权利。 所有其他商标均为其各自所有者的财产。
+            EVE Online、EVE 标志、EVE 及所有相关标志和设计均为 CCP hf 的知识产权。
+            所有与这些商标相关的知识产权的艺术作品、屏幕截图、人物、车辆、故事情节、世界事实或其他可识别的特征同样是 CCP hf 的知识产权。
+            CCP hf.已授予 :seat 使用 EVE Online 和所有相关徽标和设计的许可，用于其项目的宣传和信息目的，但不认可 SeaAT，也不以任何方式附属于 SeaAT。
+CCP对本软件的内容或功能概不负责，也不对因使用本系统而造成的任何损害承担责任。',
 
     //
     // contacts pane
     //
 
     // questions
-    'contact_pane_title' => 'Contacts',
-    'contact_pane_question_discord_first_line' => 'Have a question ? Want to say thank you ? Need to express your opinion on SeAT ?',
-    'contact_pane_question_discord_second_line' => 'Find us on Discord !',
-    'contact_pane_question_forum' => 'Alternatively, you also can track the conversation on the official :link',
-    'contact_pane_question_github' => 'Do you have a bug to report ? Please use our Github issue board :link.',
-    'contact_pane_question_eve_online_forums_placeholder' => 'Eve Online Forums',
-    'contact_pane_question_github_here_placeholder' => 'here',
+    'contact_pane_title' => '联系方式',
+    'contact_pane_question_discord_first_line' => '有什么疑问？或者想要说声谢谢？还是有关于SeAT的建议或者意见？',
+    'contact_pane_question_discord_second_line' => '请在我们的Discord服务器上找到我们！',
+    'contact_pane_question_forum' => '或者，您也可以查看我们在官方 :link 上的对话',
+    'contact_pane_question_github' => '如果你发现了bug，请使用我们的Github issue board:link。',
+    'contact_pane_question_eve_online_forums_placeholder' => 'EVE Online论坛',
+    'contact_pane_question_github_here_placeholder' => '此处',
 
     // widgets
-    'contact_widget_active_members' => ':count active members',
-    'contact_widget_join_us' => 'Join us !',
-    'contact_widget_documentation' => 'Documentation',
-    'contact_widget_updated_at' => 'Updated :date_time',
-    'contact_widget_read_me' => 'Read me !',
-    'contact_widget_github_issues' =>  ':count Issues',
-    'contact_widget_github_contribute' => 'Contribute !',
+    'contact_widget_active_members' => ':count 活跃成员人数',
+    'contact_widget_join_us' => '虚位以待！',
+    'contact_widget_documentation' => '文档',
+    'contact_widget_updated_at' => '更新于 :date_time',
+    'contact_widget_read_me' => '使用说明！',
+    'contact_widget_github_issues' =>  ':count 个问题',
+    'contact_widget_github_contribute' => '贡献者！',
 
     //
     // Donate Pane
     //
 
-    'donate_pane_title' => 'Donations',
-    'donate_pane_first_line' => 'If you like :seat_project and want to tips the community behind it, you can send ISK to
-                    :seat_holding corporation.
-                    This holding corporation is used to split donations monthly across all contributors.',
-    'donate_pane_second_line' => 'Some of it will be held as a prize pool for contents :icon.',
+    'donate_pane_title' => '捐赠',
+    'donate_pane_first_line' => '如果你喜欢:seat_project, 并想给幕后社区打赏, 您可以将 ISK 发送到
+                    :seat_holding corporation。
+                    这家军团用于每月在所有捐助方中分割捐赠。',
+    'donate_pane_second_line' => '打赏的一部分将作为奖池 :icon。',
 
     //
     // Security Pane
     //
-    'security_pane_title' => 'Security Concerns',
-    'security_pane_content' => 'If you find any security vulnerabilities within SeAT, please send an email to
-            :contact_email to address instead of creating a public Github issue.',
+    'security_pane_title' => '安全问题',
+    'security_pane_content' => '如果您在 SeAT中发现任何安全漏洞，请发送电子邮件到
+            :contact_email 地址，而不是创建一个公开的 Github issue。',
 ];
