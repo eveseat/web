eff9c1c92f896923e6a807e42aefa97025946395
Leon Jacobs <leonja511@gmail.com>
2022-08-22 20:59:49 +0200

    New translations about.php (Chinese Simplified)

--- src/resources/lang/zh-CN/about.php
+++ src/resources/lang/zh-CN/about.php
@@ -24,15 +24,15 @@ return [
     //
     // Licences Pane
     //
-    'licences_pane_title' => '许可证',
-    'licences_pane_third_party_used_licences' => 'SeAT 使用以下许可证的组件：',
-    'licences_pane_seat_used_licence' => 'SeAT 本身发布于 ：licence_link 下。',
+    'licences_pane_title' => '许可协议',
+    'licences_pane_third_party_used_licences' => 'SeAT使用了以下许可中的多个部分：',
+    'licences_pane_seat_used_licence' => 'SeAT自身是在 :licence_link 下发布的。',
     'licences_pane_ccp_third_party_licence' => '第三方许可协议',
 
     //
     // CCP Disclaimer pane
     //
-    'disclaimer_pane_title' => 'CCP 免责声明',
+    'disclaimer_pane_title' => 'CCP的免责声明',
     'disclaimer_pane_content' => ':eve_online and the EVE logo are the
             registered trademarks of :ccp_enterprise.
             All rights are reserved worldwide. All other trademarks are the property of their respective owners.
@@ -50,37 +50,37 @@ return [
     //
 
     // questions
-    'contact_pane_title' => '联系人',
-    'contact_pane_question_discord_first_line' => '有问题吗？想说谢谢吗？需要表达您对 SeAT 的看法吗？',
-    'contact_pane_question_discord_second_line' => '来我们的 Discord !',
-    'contact_pane_question_forum' => '或者，您还可以跟踪对话中的 official :link',
-    'contact_pane_question_github' => '你有错误要报告吗？请使用我们的 Github issue board :link.',
-    'contact_pane_question_eve_online_forums_placeholder' => 'Eve Online 官方论坛',
-    'contact_pane_question_github_here_placeholder' => '此处',
+    'contact_pane_title' => 'Contacts',
+    'contact_pane_question_discord_first_line' => 'Have a question ? Want to say thank you ? Need to express your opinion on SeAT ?',
+    'contact_pane_question_discord_second_line' => 'Find us on Discord !',
+    'contact_pane_question_forum' => 'Alternatively, you also can track the conversation on the official :link',
+    'contact_pane_question_github' => 'Do you have a bug to report ? Please use our Github issue board :link.',
+    'contact_pane_question_eve_online_forums_placeholder' => 'Eve Online Forums',
+    'contact_pane_question_github_here_placeholder' => 'here',
 
     // widgets
-    'contact_widget_active_members' => ':count 活跃人数',
-    'contact_widget_join_us' => '加入我们！',
-    'contact_widget_documentation' => '技术文档',
-    'contact_widget_updated_at' => '更新于 :date_time',
-    'contact_widget_read_me' => '阅读 !',
-    'contact_widget_github_issues' =>  ':count 问题',
-    'contact_widget_github_contribute' => '支持 !',
+    'contact_widget_active_members' => ':count active members',
+    'contact_widget_join_us' => 'Join us !',
+    'contact_widget_documentation' => 'Documentation',
+    'contact_widget_updated_at' => 'Updated :date_time',
+    'contact_widget_read_me' => 'Read me !',
+    'contact_widget_github_issues' =>  ':count Issues',
+    'contact_widget_github_contribute' => 'Contribute !',
 
     //
     // Donate Pane
     //
 
-    'donate_pane_title' => '赞助',
-    'donate_pane_first_line' => '如果您喜欢 :seat_project 并希望提示它背后的社区，你可以转账 ISK 到
-                    :seat_holding 公司.
-                    这家公司用于每月将捐款分给所有制作者。',
-    'donate_pane_second_line' => '其中一些将作为内容的奖池举行： :icon.',
+    'donate_pane_title' => 'Donations',
+    'donate_pane_first_line' => 'If you like :seat_project and want to tips the community behind it, you can send ISK to
+                    :seat_holding corporation.
+                    This holding corporation is used to split donations monthly across all contributors.',
+    'donate_pane_second_line' => 'Some of it will be held as a prize pool for contents :icon.',
 
     //
     // Security Pane
     //
-    'security_pane_title' => '安全问题',
-    'security_pane_content' => '如果您在 SeAT 内发现任何安全漏洞，请发送电子邮件到
-            :contact_email 而不是在 GitHub 上创建一个公开的问题。',
+    'security_pane_title' => 'Security Concerns',
+    'security_pane_content' => 'If you find any security vulnerabilities within SeAT, please send an email to
+            :contact_email to address instead of creating a public Github issue.',
 ];
