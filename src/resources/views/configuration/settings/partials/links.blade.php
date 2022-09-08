e477cb9879d6068087dc5f6cc406e50a4ee2fcd6
Crypta Eve <crypta@crypta.tech>
2022-09-08 11:22:28 +0000

    ui: use card-table for links partial

--- src/resources/views/configuration/settings/partials/links.blade.php
+++ src/resources/views/configuration/settings/partials/links.blade.php
@@ -4,62 +4,59 @@
     </div>
     <form role="form" action="{{ route('seatcore::seat.update.customlink') }}" method="post">
         {{ csrf_field() }}
-        <div class="card-body">
+        <div class="table-responsive">
+            <table class="table table-vcenter card-table">
+                <caption>
+                    <p>{!! trans('web::seat.customlink_description', [
+                        'fa-link' => '<a href="https://fontawesome.com/icons?d=gallery" target="_blank">Font Awesome</a>',
+                        'icon' => '<code>fas fa-link</code>',
+                    ]) !!}</p>
+                </caption>
+                <thead>
+                    <tr>
+                        <th>{{ trans_choice('web::seat.name', 1) }}</th>
+                        <th>{{ strtoupper(trans('web::seat.url')) }}</th>
+                        <th>Icon</th>
+                        <th>{{ trans('web::seat.new_tab_question_mark') }}</th>
+                        <th class="w-1"></th>
+                    <tr>
+                </thead>
+                <tbody id="customlinks">
 
-            <div class="table-responsive">
-                <table class="table table-vcenter">
-                    <caption>
-                        <p>{!! trans('web::seat.customlink_description', [
-                            'fa-link' => '<a href="https://fontawesome.com/icons?d=gallery" target="_blank">Font Awesome</a>',
-                            'icon' => '<code>fas fa-link</code>',
-                        ]) !!}</p>
-                    </caption>
-                    <thead>
-                        <tr>
-                            <th>{{ trans_choice('web::seat.name', 1) }}</th>
-                            <th>{{ strtoupper(trans('web::seat.url')) }}</th>
-                            <th>Icon</th>
-                            <th>{{ trans('web::seat.new_tab_question_mark') }}</th>
-                            <th class="w-1"></th>
-                        <tr>
-                        </thead>
-                    <tbody id="customlinks">
-
-                        @foreach ($custom_links as $node)
-                            <tr id="customlink{{ $loop->index }}">
-                                <td>
-                                    <input type="text" class="form-control" name="customlink-name[]"
-                                        value="{{ $node->name }}" />
-                                </td>
-                                <td>
-                                    <input type="text" class="form-control" name="customlink-url[]"
-                                        value="{{ $node->url }}" />
-                                </td>
-                                <td>
-                                    <input type="text" class="form-control" name="customlink-icon[]"
-                                        value="{{ $node->icon }}" />
-                                </td>
-                                <td>
-                                    <select class="form-select" name="customlink-newtab[]">
-                                        <option value="1" {{ $node->new_tab === false ? '' : 'selected="selected"' }}>
-                                            Yes
-                                        </option>
-                                        <option value="0" {{ $node->new_tab === false ? 'selected="selected"' : '' }}>
-                                            No
-                                        </option>
-                                    </select>
-                                </td>
-                                <td>
-                                    <button type="button" class="btn btn-danger btn-md ml-2" id="customlink-remove-btn"
-                                        onclick="customlink_remove(this)">
-                                        <i class="fas fa-trash-alt"></i>
-                                    </button>
-                                </td>
-                            </tr>
-                        @endforeach
-                    </tbody>
-                </table>
-            </div>
+                    @foreach ($custom_links as $node)
+                        <tr id="customlink{{ $loop->index }}">
+                            <td>
+                                <input type="text" class="form-control" name="customlink-name[]"
+                                    value="{{ $node->name }}" />
+                            </td>
+                            <td>
+                                <input type="text" class="form-control" name="customlink-url[]"
+                                    value="{{ $node->url }}" />
+                            </td>
+                            <td>
+                                <input type="text" class="form-control" name="customlink-icon[]"
+                                    value="{{ $node->icon }}" />
+                            </td>
+                            <td>
+                                <select class="form-select" name="customlink-newtab[]">
+                                    <option value="1" {{ $node->new_tab === false ? '' : 'selected="selected"' }}>
+                                        Yes
+                                    </option>
+                                    <option value="0" {{ $node->new_tab === false ? 'selected="selected"' : '' }}>
+                                        No
+                                    </option>
+                                </select>
+                            </td>
+                            <td>
+                                <button type="button" class="btn btn-danger btn-md ml-2" id="customlink-remove-btn"
+                                    onclick="customlink_remove(this)">
+                                    <i class="fas fa-trash-alt"></i>
+                                </button>
+                            </td>
+                        </tr>
+                    @endforeach
+                </tbody>
+            </table>
         </div>
         <div class="card-footer">
             <div class="d-flex">
