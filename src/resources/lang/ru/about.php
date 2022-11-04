e234e79ed0fce255c68aa720943834ecfac7dcbb
Leon Jacobs <leonja511@gmail.com>
2022-11-04 18:59:39 +0200

    New translations about.php (Russian)

--- src/resources/lang/ru/about.php
+++ src/resources/lang/ru/about.php
@@ -53,34 +53,34 @@ return [
     'contact_pane_title' => 'Контакты',
     'contact_pane_question_discord_first_line' => 'Есть вопросы? Хотите сказать спасибо? Нужно выразить свое мнение о SeAT ?',
     'contact_pane_question_discord_second_line' => 'Найдите нас в Discord !',
-    'contact_pane_question_forum' => 'Alternatively, you also can track the conversation on the official :link',
-    'contact_pane_question_github' => 'Do you have a bug to report ? Please use our Github issue board :link.',
-    'contact_pane_question_eve_online_forums_placeholder' => 'Eve Online Forums',
+    'contact_pane_question_forum' => 'Кроме того, вы также можете следить за нами на официальном форуме игры :link',
+    'contact_pane_question_github' => 'Нашли ошибку? Пожалуйста, сообщите о ней нам на Github :link.',
+    'contact_pane_question_eve_online_forums_placeholder' => 'Eve Online форумы',
     'contact_pane_question_github_here_placeholder' => 'сюда',
 
     // widgets
-    'contact_widget_active_members' => ':count active members',
+    'contact_widget_active_members' => ':count активных участников',
     'contact_widget_join_us' => 'Присоединяйтесь к нам!',
     'contact_widget_documentation' => 'Документация',
-    'contact_widget_updated_at' => 'Updated :date_time',
-    'contact_widget_read_me' => 'Read me !',
-    'contact_widget_github_issues' =>  ':count Issues',
-    'contact_widget_github_contribute' => 'Contribute !',
+    'contact_widget_updated_at' => 'Обновлено :date_time',
+    'contact_widget_read_me' => 'Прочитай!',
+    'contact_widget_github_issues' =>  ':count задач',
+    'contact_widget_github_contribute' => 'Внести свой вклад!',
 
     //
     // Donate Pane
     //
 
     'donate_pane_title' => 'Пожертвования',
-    'donate_pane_first_line' => 'If you like :seat_project and want to tips the community behind it, you can send ISK to
-                    :seat_holding corporation.
-                    This holding corporation is used to split donations monthly across all contributors.',
-    'donate_pane_second_line' => 'Some of it will be held as a prize pool for contents :icon.',
+    'donate_pane_first_line' => 'Если вам нравится :seat_project и вы хотите принять участием в его поддержке, вы можете отправить ISK
+                    :seat_holding корпорации.
+                    Эта корпорация используется для разделения пожертвований ежемесячно на всех участников.',
+    'donate_pane_second_line' => 'Некоторые из них будут удержаны в качестве призового фонда за содержимое :icon.',
 
     //
     // Security Pane
     //
-    'security_pane_title' => 'Security Concerns',
-    'security_pane_content' => 'If you find any security vulnerabilities within SeAT, please send an email to
-            :contact_email to address instead of creating a public Github issue.',
+    'security_pane_title' => 'Проблема с безопасностью',
+    'security_pane_content' => 'Если вы обнаружили какие-либо уязвимости безопасности в SeAT, пожалуйста, отправьте письмо на
+            :contact_email  вместо добавления поста на Github.',
 ];
