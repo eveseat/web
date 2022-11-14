6c4794b4e39957b7b84c88f3445549ce1818b51a
recursive_tree <recursivetreemail@gmail.com>
2022-11-14 20:11:08 +0100

    artisan command validation
    command arguments in schedule
    
    styleci+comments

--- src/WebServiceProvider.php
+++ src/WebServiceProvider.php
@@ -298,6 +298,7 @@ class WebServiceProvider extends AbstractSeatPlugin
     {
 
         Validator::extend('cron', 'Seat\Web\Http\Validation\Custom\Cron@validate');
+        Validator::extend('artisan', 'Seat\Web\Http\Validation\Custom\ArtisanCommand@validate');
     }
 
     /**
