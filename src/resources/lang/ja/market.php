4854f43f709a5d1ccc21e51749713008981678c9
Leon Jacobs <leonja511@gmail.com>
2022-11-24 09:31:20 +0200

    New translations market.php (Japanese)

--- src/resources/lang/ja/market.php
+++ src/resources/lang/ja/market.php
@@ -21,14 +21,14 @@
  */
 
 return [
-    'issued'  => 'Issued',
-    'expires' => 'Expires',
-    'buy'     => 'Buy',
-    'sell'    => 'Sell',
-    'order'   => 'Order',
-    'range'   => 'Range',
-    'volume'  => 'Volume',
-    'price'   => 'Price',
-    'total'   => 'Total',
-    'type'    => 'Type',
+    'issued'  => '発行済み',
+    'expires' => '有効期限',
+    'buy'     => '購入',
+    'sell'    => '売却',
+    'order'   => '注文',
+    'range'   => 'ジャンプ',
+    'volume'  => '数量',
+    'price'   => '価格',
+    'total'   => '合計',
+    'type'    => '種類',
 ];
