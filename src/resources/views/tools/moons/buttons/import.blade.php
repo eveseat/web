3cb9f8f65370cc1c9f98b538181105f5d40ba112
recursive_tree <recursivetreemail@gmail.com>
2023-04-29 22:29:25 +0200

    finish adding translations

--- src/resources/views/tools/moons/buttons/import.blade.php
+++ src/resources/views/tools/moons/buttons/import.blade.php
@@ -1,3 +1,3 @@
 <button type="button" class="btn btn-sm btn-success" data-toggle="modal" data-target="#moon-import">
-  <i class="fas fa-plus-square"></i> Import
+  <i class="fas fa-plus-square"></i> {{ trans('web::moons.import') }}
 </button>
