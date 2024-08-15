409ca730f402dcbd2861cb05075c7c88f49ae62c
recursivetree <60423027+recursivetree@users.noreply.github.com>
2024-08-15 15:23:43 +0200

    add scheduling rule migration

---
+++ src/database/migrations/2024_08_15_create_character_scheduling_rules_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use \Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration {
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('character_scheduling_rules', function (Blueprint $table) {
+            $table->bigIncrements('id')->primary();
+            $table->string("name");
+            $table->integer("interval")->unsigned();
+            $table->json("filter");
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('character_scheduling_rules');
+    }
+};
