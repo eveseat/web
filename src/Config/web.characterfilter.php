8092a2302084ca40bc9f8c48294749087cbd7ad6
Crypta Eve <crypta@crypta.tech>
2025-03-10 12:32:41 +1030

    fix: faction filter use correct path (#704)

--- src/Config/web.characterfilter.php
+++ src/Config/web.characterfilter.php
@@ -25,7 +25,7 @@ return [
     ['name' => 'character', 'src' => 'seatcore::fastlookup.characters', 'path' => '', 'field' => 'character_id', 'label' => 'Character'],
     ['name' => 'title', 'src' => 'seatcore::fastlookup.titles', 'path' => 'titles', 'field' => 'id', 'label' => 'Title'],
     ['name' => 'corporation', 'src' => 'seatcore::fastlookup.corporations', 'path' => 'affiliation', 'field' => 'corporation_id', 'label' => 'Corporation'],
-    ['name' => 'factions', 'src' => 'seatcore::fastlookup.factions', 'path' => 'affiliation', 'field' => 'corporation_id', 'label' => 'Faction'],
+    ['name' => 'factions', 'src' => 'seatcore::fastlookup.factions', 'path' => 'affiliation', 'field' => 'faction_id', 'label' => 'Faction'],
     ['name' => 'alliance', 'src' => 'seatcore::fastlookup.alliances', 'path' => 'affiliation', 'field' => 'alliance_id', 'label' => 'Alliance'],
     ['name' => 'skill', 'src' => 'seatcore::fastlookup.skills', 'path' => 'skills', 'field' => 'skill_id', 'label' => 'Skill'],
     ['name' => 'skill_level', 'src' => [['id' => 1, 'text' => 'Level 1'], ['id' => 2, 'text' => 'Level 2'], ['id' => 3, 'text' => 'Level 3'], ['id' => 4, 'text' => 'Level 4'], ['id' => 5, 'text' => 'Level 5']], 'path' => 'skills', 'field' => 'trained_skill_level', 'label' => 'Skill Level'],
