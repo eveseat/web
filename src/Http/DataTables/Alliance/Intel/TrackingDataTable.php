cbec84e875734b0ce83e7b4ecc1be293c08c0550
Chris King <raikiasec@protonmail.com>
2025-11-17 05:23:48 -0600

    Fix Alliance Tracking search (#717)
    'tracking' is not a searchable field because it is not a SQL column.  This fixes the search on the Alliance Tracking page  so its functional again.
    
    Also to note, this should resolve https://github.com/eveseat/seat/issues/838 since the tracking field is no longer orderable (as of commit b2e6520 in 2023) and now not searchable (although it never worked)

--- src/Http/DataTables/Alliance/Intel/TrackingDataTable.php
+++ src/Http/DataTables/Alliance/Intel/TrackingDataTable.php
@@ -108,7 +108,7 @@ class TrackingDataTable extends DataTable
             ['data' => 'ceo.name', 'title' => trans('web::seat.ceo')],
             ['data' => 'tax_rate', 'title' => trans('web::seat.tax_rate')],
             ['data' => 'member_count', 'title' => trans('web::seat.member_count')],
-            ['data' => 'tracking', 'title' => trans_choice('web::seat.valid_token', 2), 'orderable' => false],
+            ['data' => 'tracking', 'title' => trans_choice('web::seat.valid_token', 2), 'orderable' => false, 'searchable' => false],
         ];
     }
 }
